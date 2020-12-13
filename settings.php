3b29ced18b6b231321aebccfc75698f9588cf7cb
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2020-12-13 14:21:19 +0100

    Update settings.php

--- settings.php
+++ settings.php
@@ -2,7 +2,7 @@
 $title = 'Studio Twórców';
 require_once('partials/navbar.php');
 ?>
-	<div class="container" style="margin-top:60px;">
+	<div class="container" style="margin-top:80px;">
         <div class="row">
           <div class="col-lg-12" style="display: -webkit-box;display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;flex-direction: column;-webkit-box-align: center;align-items: center;-webkit-box-pack: center;justify-content: center;"></div>
         </div>
