daddd68940719fa72df13f505ebe51c76a8a3712
MinecraftCiach <ciach795@gmail.com>
2020-12-13 15:08:35 +0100

    Update navbar.php

--- partials/navbar.php
+++ partials/navbar.php
@@ -96,8 +96,8 @@ if ($_SESSION['z1'] == true) {
         <nav class="uk-navbar-container" uk-navbar>
             <div class="uk-navbar-left">
                 <ul class="uk-navbar-nav">
-                    <li class="uk-active"><a href=""></a></li>
-                    <li class="uk-parent"><a href=""></a></li>
+                    <li class="uk-active"><a href="#">link 1</a></li>
+                    <li class="uk-parent"><a href="#">link 2</a></li>
                     <li><a href=""></a></li>
                 </ul>
             </div>
