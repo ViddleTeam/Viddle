70e2c6cb2f582bdcb5e616ee69daa847ec7fd32d
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2020-12-13 10:03:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -100,6 +100,9 @@ require_once('partials/navbar.php');
                 </div>
             </div>
         </div>
+<?php 
+require_once('partials/footer.php');
+?>
 <!-- JS -->
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
