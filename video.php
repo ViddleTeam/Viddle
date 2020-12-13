f220070d4690def3875c227f02c616b79b036b7a
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2020-12-13 13:36:01 +0100

    Update video.php

--- video.php
+++ video.php
@@ -43,7 +43,7 @@ else
 <html lang="pl-PL"><head>
 	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?php if(isset($title)) echo $title ?></title>
+    <title><?php if(isset($title)) echo $title ?> na Viddle</title>
     <link rel="stylesheet" href="https://cdn.patryqhyper.pl/vdp/mdb/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.patryqhyper.pl/vdp/mdb/css/mdb.min.css">
     <link rel="stylesheet" href="style.css">
@@ -63,53 +63,9 @@ else
           </div>
         </div>
         <div style="opacity: 1;" class="website">
-    <header>
-  <nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar top-nav-collapse" style="height: fit-content; background-color: #212121;">
-    <a class="navbar-brand" href="index.php"><img src="https://cdn.ampersandbot.pl/p1X7yQuco.png" width="120px" /></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-6"
-      aria-controls="navbarSupportedContent-6" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent-6">
-      <ul class="navbar-nav mr-auto">
-	  <div class="container row">
-        <li class="nav-item">
-          <a class="nav-link" href="index.php" title="Strona główna"><img src="https://media.discordapp.net/attachments/627764286785060899/725795384802410617/houm.png" width="20px"/> <p class="d-lg-none">Strona główna</p></a>
-		</li>
-	  </div>
-	  <div class="container row">
-        <li class="nav-item">
-          <a class="nav-link" href="trending.php" title="Popularne"><img src="https://media.discordapp.net/attachments/627764286785060899/725795329810628628/fajer.png" width="20px"/> <p class="d-lg-none">Popularne</p></a>
-        </li>
-	  </div>
-	  <div class="container row">
-        <li class="nav-item">
-          <a class="nav-link" href="discover.php" title="Odkrywaj"><img src="https://media.discordapp.net/attachments/627764286785060899/725795361268039811/dizkower.png" width="20px"/> <p class="d-lg-none">Odkrywaj</p></a>
-        </li>
-	  </div>
-      </ul>
-      <form class="form-inline" method="GET" action="search.php" style="margin-right: auto;">
-        <input id="input_search" class="form-control mr-sm-2" style="width: 24rem; margin-top: 10px;" name="q" type="text" placeholder="Szukaj w Viddle" aria-label="Szukaj w Viddle">
-      </form>
- 
-	<ul class="navbar-nav nav-flex-icons" style="margin-right: 10px;">
-	<div class="container row">
-		<li class="nav-item">
-		  <a class="nav-link" href="upload.php" title="Udostępnij film na VDP"><img src="https://media.discordapp.net/attachments/627873018990952448/726773229863305276/AAAAA.png" width="20px" style="color: white;" /> <p class="d-lg-none">Udostępnij film na VDP</p></a>
-		</li>
-	</div>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img width="32px" style="border-radius:50%;margin-right:5px;" class="img-responsive" src="https://cdn.discordapp.com/avatars/645314415578841101/694defff96f3fe53f85260af628f3a7c.png">SlaVistaPL</a>
-                    <div class="dropdown-menu dropdown-menu-right dropdown-default" aria-labelledby="navbarDropdownMenuLink">
-                      <a class="dropdown-item waves-effect waves-light" href="channel.php">Przejdź na kanał</a>
-                      <a class="dropdown-item waves-effect waves-light" href="#">Studio twórców</a>
-                      <a class="dropdown-item waves-effect waves-light" href="#">Wyloguj się</a>
-                    </div>
-                </li>
-            </ul>   
-			</div>
-  </nav>
-      </header>
+    	<?php
+	require_once('partials/navbar.php');	
+	?>
       <div class="container" style="margin-top: 70px; justify-content: center;">
         <form>
           <div class="form-row">
