27704c4234e31f9344ba647d802650b8413772b4
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2020-12-13 14:14:59 +0100

    Update navbar.php

--- partials/navbar.php
+++ partials/navbar.php
@@ -74,7 +74,7 @@ if ($_SESSION['z1'] == true) {
                     </div>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img width="32px" style="border-radius:50%;margin-right:5px;" class="img-responsive" src="<?php echo $avatar ?>"></a>
-                        <div class="dropdown-menu dropdown-menu-right dropdown-default" style="min-width: 120px;" aria-labelledby="navbarDropdownMenuLink">
+                        <div class="dropdown-menu dropdown-menu-right dropdown-default" style="min-width: 150px;" aria-labelledby="navbarDropdownMenuLink">
                             <?php if ($avatar == 'anonim.png') { ?>
                                 <a class="dropdown-item waves-effect waves-light" href="login.php">Zaloguj się</a>
                             <?php } else { ?>
