963c218b3480bb668b11fbf78b66054311a25e15
MinecraftCiach <ciach795@gmail.com>
2020-12-13 02:33:00 +0100

    Update register.php

--- partials/navbar.php
+++ partials/navbar.php
@@ -61,7 +61,7 @@ if ($_SESSION['z1'] == true) {
                 <ul class="navbar-nav nav-flex-icons" style="margin-right: 10px;">
                     <div class="container row">
                         <li class="nav-item">
-                            <a class="nav-link" href="upload.php" title="Udostępnij film na VDP"><img src="https://media.discordapp.net/attachments/627873018990952448/726773229863305276/AAAAA.png" width="20px" style="color: white;" /> <span class="d-lg-none">Udostępnij film na VDP</span></a>
+                            <a class="nav-link" href="upload.php" title="Udostępnij film na Viddle"><img src="https://media.discordapp.net/attachments/627873018990952448/726773229863305276/AAAAA.png" width="20px" style="color: white;" /> <span class="d-lg-none">Udostępnij film na Viddle</span></a>
                         </li>
                     </div>
                     <li class="nav-item dropdown">
@@ -80,3 +80,6 @@ if ($_SESSION['z1'] == true) {
             </div>
         </nav>
     </header>
+        <?php
+        echo $_SERVER['REQUEST_URI'];
+        ?>
