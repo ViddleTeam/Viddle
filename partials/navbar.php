e1825600641f81e2f4ad209d71d6efa7f4b5cf9a
MinecraftCiach <ciach795@gmail.com>
2020-12-13 14:15:25 +0100

    Update login.php

--- partials/navbar.php
+++ partials/navbar.php
@@ -6,7 +6,7 @@ if ($_SESSION['z1'] == true) {
     $connect = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME);
     $uid = $connect->query("SELECT uid FROM viddle_users WHERE login = '%s'", $connect->real_escape_string($_SESSION['login']));
     $connect->close();
-    echo "<script>console.log('$uid')</script>";
+    echo "<script>console.log('${$_SESSION['user']}')</script>";
 } else {
     $avatar = 'anonim.png';
 }
