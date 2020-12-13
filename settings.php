ca349999a114a1a18fee3df6993f0683820640dc
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2020-12-13 14:24:13 +0100

    Update settings.php

--- settings.php
+++ settings.php
@@ -7,7 +7,7 @@ require_once('partials/navbar.php');
           <div class="col-lg-12" style="display: -webkit-box;display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;flex-direction: column;-webkit-box-align: center;align-items: center;-webkit-box-pack: center;justify-content: center;"></div>
         </div>
         <h4>Ustawienia konta</h4>
-        <div class="card-settings border border-white" style="padding: 5px;">
+        <div class="card-settings" style="padding: 5px;">
 		<p><b>Ustawienia ogólne</b></p><br>
 		
         </div>
