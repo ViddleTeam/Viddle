57061fcbb0d5f683ac5b3fa1bd1f9efa88535f66
MinecraftCiach <ciach795@gmail.com>
2020-12-13 15:11:51 +0100

    Update navbar.php

--- partials/navbar.php
+++ partials/navbar.php
@@ -93,7 +93,7 @@ if ($_SESSION['z1'] == true) {
     </header>
 -->
     <header>
-        <nav class="uk-navbar-container" uk-navbar>
+        <nav class="uk-navbar-container" uk-navbar style="background: #252525;">
             <div class="uk-navbar-left">
                 <ul class="uk-navbar-nav">
                     <li class="uk-active">
