0c8f573c801ab5de0c7afdfebead3260d8fd1f87
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2020-12-13 14:19:48 +0100

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,24 @@
+<?php
+$title = 'Studio Twórców';
+require_once('partials/navbar.php');
+?>
+	  <div class="container" style="margin-top:30px;">
+        <div class="row">
+          <div class="col-lg-12" style="display: -webkit-box;display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;flex-direction: column;-webkit-box-align: center;align-items: center;-webkit-box-pack: center;justify-content: center;"></div>
+        </div>
+        <h4>Ustawienia konta</h4>
+        <div class="card border border-white" style="padding: 5px;">
+        
+        </div>
+    </div>
+		
+		<?php 
+require_once('partials/footer.php');
+?>
+<!-- JS -->
+<script src="https://cdn.patryqhyper.pl/vdp/mdb/js/jquery.min.js"></script>
+<script src="https://cdn.patryqhyper.pl/vdp/mdb/js/bootstrap.min.js"></script>
+<script src="https://cdn.patryqhyper.pl/vdp/mdb/js/mdb.min.js"></script>
+<script src="script.js"></script>
+
+<div class="hiddendiv common"></div></body></html>
