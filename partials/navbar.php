155415893638b8c37bf67f32e9ca58b0893b795a
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2020-12-13 18:04:54 +0100

    Update navbar.php

--- partials/navbar.php
+++ partials/navbar.php
@@ -16,7 +16,7 @@ $ttl = isset($title) && $cond ? "Viddle - $title" : 'Viddle - Viddle';
 <html lang="pl-PL"><head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?php echo $ttl ?></title>
+    <title><?php echo 'czy jest dobrze' ?></title>
     <link rel="stylesheet" href="https://cdn.patryqhyper.pl/vdp/mdb/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.patryqhyper.pl/vdp/mdb/css/mdb.min.css">
     <!-- UIkit CSS -->
