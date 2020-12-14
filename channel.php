297e658724e04fc4b2cdfc60619ddc242fe6ef86
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2020-12-14 10:08:35 +0100

    Update channel.php

--- channel.php
+++ channel.php
@@ -76,14 +76,16 @@ if ($result = @$connect->query(
 					<h3 style="align-items: center;"><?php echo $nazwa ?></h3>
 					<p style="text-align: left; margin-bottom: 20px;"><?php echo $obserwatorzy ?> obserwujących</p>
 				</span>
-				<span style="margin-left: auto; display: inline-block;">
-					<?php
-					if($do == '1')
-					{
-						echo '<button type="button" class="btn btn-primary d-none d-md-block" style="padding: 10px;">Dostosuj kanał</button>';
-					}
-					?>
-					<button type="button" class="btn btn-primary" style="padding: 10px;">Obserwuj</button>
+				<span style="margin-left: auto;" class="container">
+					<div class="row">
+						<?php
+						if($do == '1')
+						{
+							echo '<button type="button" class="btn btn-primary d-none d-md-block" style="padding: 10px;">Dostosuj kanał</button>';
+						}
+						?>
+						<button type="button" class="btn btn-primary" style="padding: 10px;">Obserwuj</button>
+					</div>
 				</span>
 				</div>
 				
