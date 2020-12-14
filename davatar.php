85a5867602504ef156c462db53a2568fa8f0465b
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2020-12-14 22:07:32 +0100

    Update davatar.php

--- davatar.php
+++ davatar.php
@@ -1,1 +1,32 @@
+<?php
 
+echo 'prosze czekać ...';
+
+?>
+
+<?php
+
+session_start();
+
+if($_SESSION['uid'] == true)
+{
+  require "danesql.php";
+  $connect = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME);
+
+  $success = $connect->query("UPDATE viddle_users SET avatarname='x' WHERE id='$_SESSION['uid']'");
+  
+  if ($success) 
+  {
+    $_SESSION['ac'] = true;
+    header('location: acfinish.php');
+  }
+                    
+}
+else
+{
+  header('location: index.php');
+}
+
+
+
+?>
