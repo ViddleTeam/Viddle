eb6936057363493ceb160679c4aaba1908e8260f
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2020-12-14 22:21:56 +0100

    Update davatar.php

--- davatar.php
+++ davatar.php
@@ -12,8 +12,10 @@ if($_SESSION['uid'] == true)
 {
   require "danesql.php";
   $connect = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME);
+  
+  $uid = $_SESSION['uid'];
 
-  $success = $connect->query("UPDATE viddle_users SET avatarname='x' WHERE id='$_SESSION['uid']'");
+  $success = $connect->query("UPDATE viddle_users SET avatarname='x' WHERE id='$uid'");
   
     $_SESSION['ac'] = true;
     header('location: acfinish.php');
