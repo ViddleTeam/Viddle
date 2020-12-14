e431a031d871774820ba5aba749de33404d580b7
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2020-12-14 16:04:20 +0100

    Update navbar.php

--- partials/navbar.php
+++ partials/navbar.php
@@ -56,7 +56,6 @@ $ttl = isset($title) && $cond ? "Viddle - $title" : 'Viddle - Viddle';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $ttl ?></title>
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
