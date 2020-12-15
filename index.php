699686cf28bc911810c542401c76b0c4757c9c6c
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2020-12-15 14:30:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -30,12 +30,12 @@ require_once('partials/navbar.php');
                         <img src='https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg' class='img-responsive card-img'>
                         <p class='card-title'>Testowa nazwa</p>
                         <div class='hr' style='margin-top:-5px;margin-bottom:5px;'></div>
-                        </a><div class='bottom-info'><a href='video.php'>
-                            </a><a href='channel.php'><span>PatryQHyper</span></a>
+                        <div class='bottom-info'>
+                            <span>PatryQHyper</span></a>
                             <span>•</span>
                             <span>1.3k wyświetleń</span>
                         </div>
-                        
+			</a>
                     </div>
                     <div class='card'>
                         <a href='video.php'>
@@ -62,42 +62,42 @@ require_once('partials/navbar.php');
               </div>
             </div>
             <div class="tile" style="margin: auto;">
-                <div class="card">
-                    <a href="video.php">
-                    <img src="https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg" class="img-responsive card-img">
-                    <p class="card-title">Pierwszy film</p>
-                    <div class="hr" style="margin-top:-5px;margin-bottom:5px;"></div>
-                    <div class="bottom-info">
-                        <span>Kohady</span>
-                        <span>•</span>
-                        <span>17.5k wyświetleń</span>
+                <div class='card'>
+                        <a href='video.php'>
+                        <img src='https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg' class='img-responsive card-img'>
+                        <p class='card-title'>Pierwszy film</p>
+                        <div class='hr' style='margin-top:-5px;margin-bottom:5px;'></div>
+                        <div class='bottom-info'>
+                            <span>Kohady</span>
+                            <span>•</span>
+                            <span>17.5k wyświetleń</span>
+                        </div>
+                        </a>
                     </div>
-                    </a>
-                </div>
-                <div class="card">
-                    <a href="video.php">
-                    <img src="https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg" class="img-responsive card-img">
-                    <p class="card-title">Testowa nazwa</p>
-                    <div class="hr" style="margin-top:-5px;margin-bottom:5px;"></div>
-                    </a><div class="bottom-info"><a href="video.php">
-                        </a><a href="channel.php"><span>PatryQHyper</span></a>
-                        <span>•</span>
-                        <span>1.3k wyświetleń</span>
+                    <div class='card'>
+                        <a href='video.php'>
+                        <img src='https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg' class='img-responsive card-img'>
+                        <p class='card-title'>Testowa nazwa</p>
+                        <div class='hr' style='margin-top:-5px;margin-bottom:5px;'></div>
+                        <div class='bottom-info'>
+                            <span>PatryQHyper</span></a>
+                            <span>•</span>
+                            <span>1.3k wyświetleń</span>
+                        </div>
+			</a>
                     </div>
-                    
-                </div>
-                <div class="card">
-                    <a href="video.php">
-                    <img src="https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg" class="img-responsive card-img">
-                    <p class="card-title">Zrobione z nudów</p>
-                    <div class="hr" style="margin-top:-5px;margin-bottom:5px;"></div>
-                    <div class="bottom-info">
-                        <span>Hekitu</span>
-                        <span>•</span>
-                        <span>9k wyświetleń</span>
+                    <div class='card'>
+                        <a href='video.php'>
+                        <img src='https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg' class='img-responsive card-img'>
+                        <p class='card-title'>Zrobione z nudów</p>
+                        <div class='hr' style='margin-top:-5px;margin-bottom:5px;'></div>
+                        <div class='bottom-info'>
+                            <span>Hekitu</span>
+                            <span>•</span>
+                            <span>9k wyświetleń</span>
+                        </div>
+                        </a>
                     </div>
-                    </a>
-                </div>
             </div>
         </div>
 <?php 
