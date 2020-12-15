0bc99d8383058c861234b997a89f897fd30390fa
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2020-12-15 16:04:38 +0100

    Update settings.php

--- settings.php
+++ settings.php
@@ -15,7 +15,7 @@ require_once('partials/navbar.php');
       aria-selected="false">Strefa niebezpieczna</a>
   </li>
 </ul>
-<div class="tab-content" id="myTabContent" style="padding-top: 10px;">
+<div class="tab-content" id="myTabContent" style="padding-top: 10px; color: white !important;">
   <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab"><table class="table">
   <thead>
     <tr>
@@ -29,21 +29,21 @@ require_once('partials/navbar.php');
             <b>Zmień nazwę konta</b><br>
             <p>Nie podoba się Tobie obecna nazwa? Możesz ją zmienić tutaj.</p>
         </th>
-        <td><button type="button" class="btn btn-primary">Zmień nazwę konta</button></td>
+        <td><button type="button" class="btn btn-primary" style="padding: 10px;">Zmień nazwę konta</button></td>
     </tr>
     <tr>
         <th scope="row">
             <b>Zmień hasło</b><br>
             <p>Uważasz, że Twoje obecne hasło jest zbyt słabe? Ustaw tutaj silniejsze!</p>
         </th>
-        <td><button type="button" class="btn btn-primary">Zmień hasło</button></td>
+        <td><button type="button" class="btn btn-primary" style="padding: 10px;" >Zmień hasło</button></td>
     </tr>
     <tr>
         <th scope="row">
             <b>Weryfikacja dwuetapowa <span class="badge badge-warning">Już wkrótce</span></b><br>
             <p>Silne hasło Twoim zdaniem nie wystarczy? Włącz weryfikację dwuetapową, żeby jeszcze bardziej chronić swoje konto.</p>
         </th>
-        <td><button type="button" class="btn btn-primary">Włącz 2FA</button></td>
+        <td><button type="button" class="btn btn-primary" style="padding: 10px;">Włącz 2FA</button></td>
     </tr>
   </tbody>
 </table></div>
