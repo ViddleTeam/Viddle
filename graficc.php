a9ad815db489e17c77051397754165cf42f5bc96
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2020-12-15 11:19:20 +0100

    Update graficc.php

--- graficc.php
+++ graficc.php
@@ -53,12 +53,13 @@ require_once('partials/navbar.php');
         </div>
           <div class="row">
               <div class="col-lg-12">
-                  <h4 class="tile-before" style="color:white; margin-top: 40px;"><br>Zmień grafikę swojego kanału</h4><br><br>
+                  <h4 class="tile-before" style="color:white; margin-top: 40px;"><br>Zmień grafikę swojego kanału</h4><br>
+		      <p>Żeby zmienić awatar, kliknij na przycisk znajdujący się pod Twoim aktualnym awatarem. Aktualizacja zdjęcia profilowego może potrwać do kilku minut.</p><br>
 		      <center>
 			      <img src="http://wallpapercave.com/wp/t05PXKg.jpg" width="100%" height="100%" style="border-radius: 10px; box-shadow: 0 0 15px -5px black;" />
 			      <div class="card-channel" style="border-radius: 0 0 10px 10px; margin-top: -100px; margin-left: 0px; margin-right: 0px; width: 100%; height: auto; padding: 20px;">
 				<img width="204px" style="border-radius:50%; margin-bottom:5px;" class="img-responsive" src="<?php echo $av4 ?>"><br>
-		      		<a href="achange.php"><button type="button" class="btn btn-secondary"><p style="margin: 10px;">Zmień avatar</p></button></a>
+		      		<a href="achange.php"><button type="button" class="btn btn-secondary"><p style="margin: 10px;">Zmień awatar</p></button></a>
 			      </div>
 		      </center>
 								
