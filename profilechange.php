d0239ec17ada623100961a55b99e2fce0852ffb8
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2020-12-17 16:18:18 +0100

    Update profilechange.php

--- profilechange.php
+++ profilechange.php
@@ -90,9 +90,7 @@ require_once('partials/footer.php');
 	      <p>Wybierz plik obrazu, który posłuży jako zdjęcie profilowe na Viddle.</p><br>
 	      <center> <img width="204px" style="border-radius:50%;margin-right:5px;" class="img-responsive" src="<?php echo $av4 ?>"> <br></br></center>
 		<center>
-			<input type="file" name="file_picker" value="Wybierz plik" hidden />
-			<label for="file_picker" class="btn btn-primary" id="actual-btn" style="padding: 10px;">Wybierz zdjęcie</label>
-			<span id="file-chosen" style="color: white;">Nie wybrano pliku</span>
+			<input type="file" name="file_picker" class="btn btn-primary" value="Wybierz plik" hidden />
 		</center>
       </div>
       <div class="modal-footer">
@@ -124,14 +122,6 @@ require_once('partials/footer.php');
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
-<script>
-const actualBtn = document.getElementById('file_picker');
-const fileChosen = document.getElementById('file-chosen');
-
-actualBtn.addEventListener('change', function(){
-  fileChosen.textContent = this.files[0].name
-})
-</script>
 <?php
 if ($_SESSION['z1'] == false) {
 	echo "<script>$('#staticBackdrop').modal('show');</script>";
