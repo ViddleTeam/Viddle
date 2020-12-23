ebeb42166f8708a356654025c627ea35dc1000c7
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2020-12-23 18:27:42 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -16,6 +16,9 @@ input[type=submit] {
           <div class="col-lg-12" style="display: -webkit-box;display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;flex-direction: column;-webkit-box-align: center;align-items: center;-webkit-box-pack: center;justify-content: center;">
         </div>
         </div>
+	      <div class="alert alert-warning">
+	      	Uwaga: ze względu na limity hostingu, limit jednego filmu na Viddle obecnie wynosi 10 MB. <a href="https://support.infinityfree.net/files/how-to-upload-big-files-archives/">Dlaczego?</a>
+	      </div>
           <div class="row">
               <div class="col-lg-12">
                   <h4 class="tile-before" style="color:white; margin-top: 40px;"><br>Udostępnij film na Viddle</h4>
@@ -28,7 +31,7 @@ input[type=submit] {
 						<p style="align-items: center; color: white;">
 							<center>
 								<h2 style="color: white;">Wybierz plik wideo z komputera/telefonu</h2>
-                <p style="color: white;">Maksymalny dozwolony rozmiar na jeden film wynosi 1 GB. Chcesz się dowiedzieć, ile dany film może ważyć? Użyj <a href="https://toolstud.io/video/filesize.php?">tego narzędzia.</a></p>
+                <p style="color: white;">Maksymalny dozwolony rozmiar na jeden film wynosi 10 MB. Chcesz się dowiedzieć, ile dany film może ważyć? Użyj <a href="https://toolstud.io/video/filesize.php?">tego narzędzia.</a></p>
                 <p style="color: white; font-weight: bold; margin-top: -10px;">Pamiętaj, że administracja Viddle ma pełne prawo do usunięcia filmu, jeżeli narusza on prawa autorskie i/lub regulamin.</p>
 								<form action="/post.php" method="post">
 								<input type="file" accept="video/mp4, video/mov" name="videovid" style="color: white; margin-top: 5px;" /> 
