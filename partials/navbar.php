72170655090a7ea6f8a8a70ef2ae6fba383b174b
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2020-12-23 08:33:15 +0100

    Update navbar.php

--- partials/navbar.php
+++ partials/navbar.php
@@ -53,7 +53,6 @@ $cond = strpos($url, $str) !== false;
 $ttl = isset($title) && $cond ? "Viddle - $title" : 'Viddle - Viddle';
 ?>
 <html lang="pl-PL"><head>
-    <script async src="https://arc.io/widget.min.js#YXGBLYWj"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $ttl ?></title>
