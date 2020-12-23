865ed9bedb5392e658028c1214d5bd214b0c4935
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2020-12-23 08:34:04 +0100

    Update navbar.php

--- partials/navbar.php
+++ partials/navbar.php
@@ -55,6 +55,7 @@ $ttl = isset($title) && $cond ? "Viddle - $title" : 'Viddle - Viddle';
 <html lang="pl-PL"><head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script data-ad-client="ca-pub-4393741826344878" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <title><?php echo $ttl ?></title>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
