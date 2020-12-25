eb1b9df3a0c1322a3dc4d80c08200f91fbaa1510
Lort533 <76491869+Lort533@users.noreply.github.com>
2020-12-26 00:53:38 +0100

    Update post.php

--- post.php
+++ post.php
@@ -88,7 +88,7 @@ if ($_SESSION['z1'] == true) {
   $viddlepath = $viddleid;
   $viddlepath .= ".mp4";
   $data = date("Y-m-d H:i:s");
-  $success = $connect->query("INSERT INTO viddle_videos VALUES ('$login', 123454321, '$viddleid', 0, 0, 0, 0, '$newfilename', '$tytul', '$opis', 'x', '$data')");
+  $success = $connect->query("INSERT INTO viddle_videos VALUES ('$login', 123454321, '$viddleid', 0, 0, 0, 0, '$file_ext', '$tytul', '$opis', 'x', '$data')");
   if ($success) {
      header('Location: video.php?id=' . $viddleid);
   } else {
