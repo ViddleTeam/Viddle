451e48119068ec9785d1d7c38af86bc004dee683
MinecraftCiach <ciach795@gmail.com>
2020-12-26 14:01:49 +0100

    Add 413 error prototype

--- index.php
+++ index.php
@@ -116,11 +116,6 @@ require_once('partials/navbar.php');
     </div>
   </div>
 </div>
-<!-- JS -->
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
 <?php 
 require_once('partials/footer.php');
 ?>
