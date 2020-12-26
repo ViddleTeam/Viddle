dd1644c4f0c0b80de76d424c20c4062e74e1126c
MinecraftCiach <ciach795@gmail.com>
2020-12-26 13:51:32 +0100

    Add 413 error prototype

--- file_too_large.php
+++ file_too_large.php
@@ -1,4 +1,28 @@
 <?php
 require_once('partials/navbar.php');
 ?>
-
+    <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content bg-dark">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Wybrany plik jest zbyt duży</h5>
+                </div>
+                <div class="modal-body">
+                    Przepraszamy, ale wybrany plik ("<?php echo $_POST["titlevid"] ?>") jest zbyt duży, aby można było go wrzucić na Viddle. Możesz spróbować poniższych rozwiązań, aby móc go udostępnić:
+                    <ul>
+                        <li>Skompresować plik i udostępnić go na Viddle</li>
+                        <li>Nagrać, albo zmontować go jeszcze raz, aby ważył mniej niż 10 MB.</li>
+                    </ul>
+                </div>
+                <div class="modal-footer">
+                    <a href="index.php"><button type="button" class="btn btn-primary" style="padding: 10px;">Powrót na stronę główną</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
+<script>
+    $('#staticBackdrop').modal('show');
+</script>
+<?php
+require_once('partials/footer.php');
+?>
