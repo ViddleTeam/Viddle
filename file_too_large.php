0a366684b6ff0e3acdb22aa1c4e99dd2937ec6a5
evoDesign <57808067+evolutionevotv@users.noreply.github.com>
2020-12-27 14:46:13 +0100

    Update file_too_large.php

--- file_too_large.php
+++ file_too_large.php
@@ -5,17 +5,14 @@ require_once('partials/navbar.php');
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content bg-dark">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Wybrany plik jest zbyt duży</h5>
+                    <h5 class="modal-title" id="staticBackdropLabel">Plik przekroczył limit rozmiaru</h5>
                 </div>
                 <div class="modal-body">
-                    Przepraszamy, ale wybrany plik ("<?php echo $_POST["titlevid"] ?>") jest zbyt duży, aby można było go wrzucić na Viddle. Możesz spróbować poniższych rozwiązań, aby móc go udostępnić:
-                    <ul>
-                        <li>Skompresować plik i udostępnić go na Viddle</li>
-                        <li>Nagrać, albo zmontować go jeszcze raz, aby ważył mniej niż 10 MB.</li>
-                    </ul>
+                    ("<?php echo $_POST["titlevid"] ?>") przekroczył limit rozmiaru pliku. 
+
                 </div>
                 <div class="modal-footer">
-                    <a href="index.php"><button type="button" class="btn btn-primary" style="padding: 10px;">Powrót na stronę główną</button></a>
+                    <a href="index.php"><button type="button" class="btn btn-primary" style="padding: 10px;">OK</button></a>
                 </div>
             </div>
         </div>
