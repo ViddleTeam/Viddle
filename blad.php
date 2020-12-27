d63d85220120a97d66d99f7f1c109320bdc1a7fa
Lort533 <76491869+Lort533@users.noreply.github.com>
2020-12-27 02:43:27 +0100

    Update blad.php

--- blad.php
+++ blad.php
@@ -15,11 +15,11 @@ button {
 <?php
 $id = $_GET['id'];
 if($id=="1") {
-  $test = "Wystąpił poważny błąd serwisu! Jeżeli problem nie ustąpi, skontaktuj się z deweloperami strony.<br /> <h3>Kod błędu: 0x00001c</h3>";
+  $test = "Wystąpił poważny błąd serwisu! Jeżeli problem nie ustąpi, skontaktuj się z deweloperami strony.<br /><h3>Kod błędu: 0x00001c</h3>";
 } elseif($id=="2") {
-  $test = "Twój plik jest za duży, lub użyłeś złęgo formatu pliku!<br /> Dozwolone formaty: .mp4, .wmv, .webm, .mov, .3gp<br />.Jeżeli problem nie ustąpi, skontaktuj się z deweloperami strony.<br /> <h3>Kod błędu: 0x00002c</h3>";
+  $test = "Twój plik jest za duży, lub użyłeś złęgo formatu pliku!<br />Dozwolone formaty: .mp4, .wmv, .webm, .mov, .3gp<br />Maksymalny rozmiar pliku: 10 MB<br />Jeżeli problem nie ustąpi, skontaktuj się z deweloperami strony.<br /><h3>Kod błędu: 0x00002c</h3>";
 } elseif($id=="3") {
-  $test = "Wystąpił poważny błąd serwisu! Jeżeli problem nie ustąpi, skontaktuj się z deweloperami strony.<br /> <h3>Kod błędu: 0x00003c</h3>";
+  $test = "Wystąpił poważny błąd serwisu! Jeżeli problem nie ustąpi, skontaktuj się z deweloperami strony.<br /><h3>Kod błędu: 0x00003c</h3>";
 } else {
   $test = "Heh, śmieszne. Wystąpił błąd taki, że w linku nie podano kodu błędu. Śmieszne, co nie?<br />";
 }
