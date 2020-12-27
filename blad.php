5514074694457121b87e0bf7ebec7500b0b6e0e4
evoDesign <57808067+evolutionevotv@users.noreply.github.com>
2020-12-27 15:27:19 +0100

    Update blad.php

--- blad.php
+++ blad.php
@@ -15,15 +15,18 @@ button {
 <?php
 $id = $_GET['id'];
 if($id=="1") {
-  $test = "Wystąpił poważny błąd serwisu! Jeżeli problem nie ustąpi, skontaktuj się z deweloperami strony.<br /><h3>Kod błędu: 0x00001c</h3>";
+  $test = "Kurza twarz! Wystąpił błąd serwisu. Koniecznie przekaż to naszej drużynie.
+<br /><h2>Kod błędu to 0x00001c</h2>";
 } elseif($id=="2") {
-  $test = "Twój plik jest za duży, lub użyłeś złęgo formatu pliku!<br />Dozwolone formaty: .mp4, .wmv, .webm, .mov, .3gp<br />Maksymalny rozmiar pliku: 10 MB<br />Jeżeli problem nie ustąpi, skontaktuj się z deweloperami strony.<br /><h3>Kod błędu: 0x00002c</h3>";
+  $test = "Twój plik jest jakiś troszeczkę podejrzany... lub stało się coś nieoczekiwanego<br />Wróć do poprzedniej strony i spróbuj ponownie.<br />Możliwe że to jest problem po naszej stronie, więc skontaktuj się z naszą drużyną!<br /><h2>Kod błędu to 0x00002c</h2>";
 } elseif($id=="3") {
-  $test = "Wystąpił poważny błąd serwisu! Jeżeli problem nie ustąpi, skontaktuj się z deweloperami strony.<br /><h3>Kod błędu: 0x00003c</h3>";
+  $test = "Kurza twarz! Wystąpił poważny błąd serwisu. Koniecznie przekaż to naszej drużynie.<br /><h3>Kod błędu to 0x00003c</h3>";
 } else {
-  $test = "Heh, śmieszne. Wystąpił błąd taki, że w linku nie podano kodu błędu. Śmieszne, co nie?<br />";
+  $test = "Wystąpił nieznany błąd, Koniecznie przekaż to naszej drużynie.<br />";
 }
 ?>
-<center><h1>Wystąpił błąd!</h1><br />
+<center><h1>Ups.</h1></center>
+
+
 <?php echo($test) ?>
 <a href="index.php"><p style="color: white;"><button class="btn btn-success">Powrót do strony głównej</button></p></a></center>
