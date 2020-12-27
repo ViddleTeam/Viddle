52be0775051262d45f289c99a5232e53dd33d16b
Lort533 <76491869+Lort533@users.noreply.github.com>
2020-12-27 01:55:40 +0100

    Update blad.php

--- blad.php
+++ blad.php
@@ -1,7 +1,3 @@
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@300&display=swap" rel="stylesheet">
-<style>
-font-family: 'Oxygen', sans-serif;
-</style>
+<link rel="stylesheet" href="style.css">
 <center>Jakiś powód<br />
 <a href="index.php"><button>Powrót do strony głównej</button></a></center>
