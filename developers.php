e4f04a1a3682cd91ead2eeeef990bbba074d44db
MinecraftCiach <ciach795@gmail.com>
2020-12-27 02:03:38 +0100

    Update

--- developers.php
+++ developers.php
@@ -1,9 +1,25 @@
 <?php
 require_once('partials/navbar.php');
 ?>
-<center>
-    <h1>Dla Deweloperów</h1>
-</center>
+<div class="container" style="margin-top:30px;">
+    <div class="row">
+        <div class="col-lg-12" style="display: -webkit-box;display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;flex-direction: column;-webkit-box-align: center;align-items: center;-webkit-box-pack: center;justify-content: center;">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <h4 class="tile-before" style="color:white; margin-top: 40px;"><br>Dla Deweloperów</h4><br>
+            <center>
+
+            </center>
+            </p>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12" style="display: -webkit-box;display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;flex-direction: column;-webkit-box-align: center;align-items: center;-webkit-box-pack: center;justify-content: center;">
+    </div>
+</div>
 <?php
 require_once('partials/footer.php');
 ?>
