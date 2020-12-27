a256e50c8726368e3fcb87062dc6dd1ff0f90e7a
Lort533 <76491869+Lort533@users.noreply.github.com>
2020-12-27 01:50:59 +0100

    Update blad.php

--- blad.php
+++ blad.php
@@ -1,2 +1,5 @@
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400&display=swap');
+</style>
 <center>Jakiś powód<br />
 <a href="index.php"><button>Powrót do strony głównej</button></a></center>
