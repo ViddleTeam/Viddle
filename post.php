5fd9913858538922c1554a558f7ce5b59a79a506
Lort533 <76491869+Lort533@users.noreply.github.com>
2020-12-28 22:45:15 +0100

    Update post.php

--- post.php
+++ post.php
@@ -37,6 +37,12 @@ if ($_SESSION['z1'] == true) {
   	  $zabezpdwa = mysqli_real_escape_string($connect, htmlspecialchars($tytul));
   	  $zabezptrzy = mysqli_real_escape_string($connect, htmlspecialchars($opis));
 	  $viddleid = rand(1000000,9999999);
+	  if ($rezultat = @$connect->query("SELECT * FROM viddle_recent WHERE number=1")
+          $danedwa = $rezultat->fetch_assoc();
+          $lastcheck = $danedwa['viddle_recent_one_user'];
+	  if($lastcheck==$login) {
+	    header('Location: blad.php?id=4');
+	  }
 	  if ($result = @$connect->query(sprintf("SELECT * FROM viddle_video WHERE video_id='%s", mysqli_real_escape_string($connect,$viddleid))))
 	  $d2 = $result->num_rows;
 	  if (isset($d2) && $d2 >= '1') {
