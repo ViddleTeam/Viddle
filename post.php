701d1cc65b804647d23db75b98e79f688dd1bbe3
Lort533 <76491869+Lort533@users.noreply.github.com>
2020-12-29 00:37:57 +0100

    Update post.php

--- post.php
+++ post.php
@@ -32,7 +32,7 @@ if ($_SESSION['z1'] == true) {
 	  }
 	  require "danesql.php";
   	  $connect = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME);
-	  $result = $connect -> query("SELECT * FROM `viddle_recent` WHERE number=1");
+	  $result = $connect -> query("SELECT * FROM `viddle_recent` WHERE number=1;");
 	  $row = $result -> fetch_row();
 	  if($row['viddle_recent_one_user'] == $login) {
 	    header('Location: blad.php?id=4');
