fb04bf8e01a03195f3d0ad5d3023680c5d98b12d
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2020-12-28 18:56:45 +0100

    Update post.php

--- post.php
+++ post.php
@@ -106,7 +106,7 @@ if ($_SESSION['z1'] == true) {
     	  }
 	  if (in_array($file_ext, $allowed_file_types) && ($filesize < 10*MB))
 	  {
-	  $success = $connect->mysqli_real_escape_string(htmlspecialchars(query("INSERT INTO viddle_videos VALUES ('$login', 123454321, '$viddleid', 0, 0, 0, 0, '$newfilename', '$tytul', '$opis', 'x', '$data')")));
+	  $success = $connect->mysqli_real_escape_string(htmlentities(query("INSERT INTO viddle_videos VALUES ('$login', 123454321, '$viddleid', 0, 0, 0, 0, '$newfilename', '$tytul', '$opis', 'x', '$data')")));
 	  }
 	  if ($success) {
 	     header('Location: video.php?id=' . $viddleid);
