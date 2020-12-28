30a7c1965268ec835eab853e2330efc0feb0ae09
Lort533 <76491869+Lort533@users.noreply.github.com>
2020-12-29 00:34:59 +0100

    Update post.php

--- post.php
+++ post.php
@@ -32,9 +32,9 @@ if ($_SESSION['z1'] == true) {
 	  }
 	  require "danesql.php";
   	  $connect = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME);
-	  $result = $connect->query("SELECT * FROM viddle_recent WHERE viddle_recent_one_user='$login'");
+	  if ($result = @$connect->query(sprintf("SELECT * FROM viddle_recent WHERE viddle_recent_one_user='%s", mysqli_real_escape_string($connect,$login))))
 	  $testing = $result->num_rows();
-	  if(isset($testing) && $testing >= '1') {
+	  if($testing >= 1) {
 	    header('Location: blad.php?id=4');
 	  }
 	  $test = ext($file_ext);
