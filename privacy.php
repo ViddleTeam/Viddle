dce7bf8ec03225c3ea68a1dab29e77f22ba36662
evoDesign <57808067+evolutionevotv@users.noreply.github.com>
2020-12-29 15:39:01 +0100

    Update privacy.php

--- privacy.php
+++ privacy.php
@@ -6,63 +6,10 @@ require_once('partials/navbar.php');
  <head>
  </head>
 <body>
-<center>
-<h2>POLITYKA PRYWATNOŚCI VIDDLE</h2><br>
-<br>
 
-Data wejścia w życie: 31.12.2020
- <br>
 
-1. Postanowienia ogólne
- <br>
 
-Ochrona danych osobowych ma dla nas duże znaczenie. Jeżeli przetwarzanie Twoich danych osobowych jest konieczne, ale nie ma do tego żadnej podstawy,<br> będziemy prosili o wyrażenie nieobowiązkowej zgody, <br>którą będzie można wycofać w np. ustawieniach konta.
 
-Administratorem twoich danych, jak i podmiotem przetwarzającym Twoje dane jest Viddle.
 
-Jeżeli wyrazisz zgodę na przetwarzanie Twoich danych, będziemy je przetwarzać zgodnie z ogólnym rozporządzeniem o ochronie danych osobowych, RODO (GDPR) oraz przepisach dotyczących ochrony danych obowiązujących na terytorium Rzeczypospolitej Polskiej.
-
-Poprzez politykę prywatności serwisu informujemy wszystkich użytkowników o charakterze, zakresie i celu zbierania, wykorzystywania i przetwarzania danych. Również, zgodnie z niniejszą deklaracją, użytkownik serwisu otrzymuje również informację o przysługujących mu prawach.
-
-Ponieważ internetowe transmisje danych mogą mieć luki, nie możemy zagwarantować absolutnej ochrony przekazywanych przez Ciebie danych. Do transmisji tych danych wykorzystujemy szyfrowanie SSL oraz dobrze zabezpieczone serwery.
-
-2. Jakie zbieramy dane, w jaki sposób je zbieramy oraz jak są one wykorzystywane?
-
-Dane, które zbieramy od zarejestrowanych to jedynie Twój adres mailowy. Wykorzystujemy zebrane dane do świadczenia usługi i/lub ich ulepszania, tj.: weryfikacja, czy adres mailowy należy do Ciebie, kontaktu drogą elektroniczną, do celów reklamowych i działań marketingowych. Chcemy się upewnić, że przestrzegamy wymogów prawnych.
-
-Dane, które uzyskujemy od odwiedzających witrynę to typ urządzenia i przeglądarka, których używacie, adres IP oraz informacje o plikach cookie zainstalowanych na urządzeniu.
-
-Zebrane informacje są wykorzystywane w celach, gdy wykonujesz działanie, tj. rejestracja nowego konta.
-
-3. Jak długo przechowujemy zebrane dane osobowe?
-
-Dane osobowe są przechowywane tak długo, jak to jest potrzebne do świadczenia usługi i przestrzegania zobowiązań prawnych.
-
-Masz prawo do usunięcia swoich danych całkowicie, lecz wtedy będziemy musieli zamknąć Twoje konto na Viddle.
-
-4. Jak uzyskać dostęp do danych osobowych i nimi zarządzać?
-
-Do przekazanych danych możesz mieć wgląd poprzez wysłanie żądania danych na adres mailowy support@viddle.xyz. Możesz poprosić o ich modyfikację, nanosić poprawki, ograniczać przetwarzanie lub sprzeciwić się ich przetwarzaniu w przypadkach, gdy zostałeś/aś poproszony/a o zgodę na ich przetwarzanie.
-
-Istnieje wiele przypadków, w których te prawa mogą być ograniczone i/lub w których możemy zachować dane po wycofaniu zgody.
-
-Jeżeli jednak masz wątpliwości, możesz się z nami skontaktować drogą elektroniczną (adres mailowy: support@viddle.xyz) bądź przez komunikator Discord, otwierając ticket na naszym serwerze.
-
-5. Informacje publiczne i strony internetowe osób/firm trzecich
-
-Posiadamy profile w głównych mediach społecznościowych, tj. Facebook, Twitter czy Instagram. Prosimy również nie zapominać o tym, że wszystkie informacje podane do obszarów publicznych tychże sieci mogą być łatwo odczytane, zebrane bądź zapisane przez innych użytkowników stron trzecich. Nie jesteśmy w stanie i nie możemy monitorować działań zarówno innych platform, jak i również ich użytkowników.
-
-Klikając na link w opisie np. filmu, zostaniesz przekierowany na stronę należącą do osoby/firmy trzeciej. Nie możemy jednak kontrolować działań wykonywanych przez tamtejszą witrynę, w związku z tym nie ponosimy odpowiedzialności za wykorzystanie danych osobowych przez inną stronę. Dla pewności, zapoznaj się z polityką prywatności strony internetowej, na którą przechodzisz z poziomu Viddle.
-
-6. Ochrona prywatności małoletnich
-
-Viddle jest portalem przeznaczonym dla osób powyżej 13 lat. Żadna osoba, która nie przekracza progu dozwolonego wieku, nie może posiadać konta w serwisie. Nie zbieramy żadnych danych od osób małoletnich, chyba że rodzic/opiekun prawny wyraził na to zgodę. W przypadku, gdy dowiemy się, że zasada została naruszona, konto zostanie usunięte wraz ze wszystkimi informacjami. Jeżeli jesteś rodzicem lub opiekunem prawnym, a chciałbyś zarządzać kanałem należącym do osoby małoletniej, skontaktuj się z nami poprzez drogę elektroniczną (support@viddle.xyz) lub poprzez serwer Discord, otwierając ticket.
-
-7. Postanowienia końcowe
-
-Podstawą prawną jest punkt 6. dyrektywy RODO dla przetwarzania danych, dla których uzyskujemy zgodę na konkretny cel przetwarzania.
-
-Zastrzegamy sobie prawo do zmiany lub modyfikacji niniejszej Polityki prywatności w dowolnym momencie. W przypadku wprowadzenia zmian opublikujemy je w sekcji Polityka prywatności. O wszelkich zmianach w Polityce prywatności będziemy informować przez komunikator Discord na naszym serwerze. 
-</center>
- </body>
+</body>
 </html>
