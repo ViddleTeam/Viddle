88009a9c409a7a9cd440335a2e138e260378ae87
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2020-12-29 23:30:22 +0100

    Update preset.php

--- preset.php
+++ preset.php
@@ -1,1 +1,58 @@
+<?php
 
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<style>
+		body {
+			background: rgb(30,0,59);
+			background: linear-gradient(90deg, rgba(30,0,59,1) 0%, rgba(9,34,121,1) 35%, rgba(154,0,255,1) 100%);
+		}
+		</style>
+		<link rel="stylesheet" href="http://midacss.ml/assets/master.min.css" />
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Zaloguj się do Viddle</title>
+		<link rel="stylesheet" href="https://cdn.patryqhyper.pl/vdp/mdb/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://cdn.patryqhyper.pl/vdp/mdb/css/mdb.min.css">
+		<link rel="stylesheet" href="style.css">
+		<meta property="og:title" content="Viddle">
+		<meta property="og:description" content="Filmy, muzyka i wiele więcej. Udostępniaj swoje filmy znajomym, rodzinie, oraz całemu światu, za pomocą Viddle.">
+		<script src="script.js"></script>
+		<script src="https://kit.fontawesome.com/604acb9c5e.js"></script>
+		<link rel="stylesheet" href="https://kit-free.fontawesome.com/releases/latest/css/free.min.css" media="all"><link rel="stylesheet" href="https://kit-free.fontawesome.com/releases/latest/css/free-v4-font-face.min.css" media="all"><link rel="stylesheet" href="https://kit-free.fontawesome.com/releases/latest/css/free-v4-shims.min.css" media="all">
+	</head>
+	<body class="bg-img img-fluid" style="align-items: center; justify-content: center; text-align: center;">
+			<div class="card-login" style="width: 550px; height: auto; align-items: center; padding: 15px 0 15px 0;">
+				<div class="card-body">
+				<h3 style="font-weight: bold;">Zaloguj się do Viddle</h3>
+				<p>Miło Ciebie znowu widzieć!</p><br>
+				<?php
+
+				echo $error;	
+
+				?>
+
+                <form method="post">
+
+				<div class="md-form input-group mb-3" style="margin: auto; width: 100%">
+					<input type="text"name="login" class="form-control" placeholder="Nazwa użytkownika" aria-label="Nazwa użytkownika" aria-describedby="material-addon1">
+				</div>
+				<div class="md-form input-group mb-3" style="margin: auto; width: 100%">
+					<input type="password" name="haslo" class="form-control" placeholder="Hasło" aria-label="Hasło" aria-describedby="material-addon1">
+				</div><br>
+				<center>
+				<div class="container row" style="justify-content: center;">
+				<input type="submit" value="Wyślij e-maila" class="btn btn-success" style="padding: 10px; color: white;">
+
+                </form>
+
+				</div>
+
+				</center>
+			</div>
+			</div><br>
+	</body>
+</html>
