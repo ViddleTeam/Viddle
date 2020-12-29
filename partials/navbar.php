a76878b2c9a5d2a41e05b88db58099e9f508d80e
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2020-12-29 20:34:41 +0100

    Update navbar.php

--- partials/navbar.php
+++ partials/navbar.php
@@ -33,6 +33,7 @@ $ttl = isset($title) && $cond ? "Viddle - $title" : 'Viddle - Viddle';
 <html lang="pl-PL"><head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="/logo (1).png">
     <script data-ad-client="ca-pub-4393741826344878" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <title><?php echo $ttl ?></title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
