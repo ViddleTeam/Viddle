8edf73e4555692358ae04c764cefd772ae6ae7b9
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2020-12-29 09:48:02 +0100

    Update video.php

--- video.php
+++ video.php
@@ -90,7 +90,7 @@ if ($id == 0) {
                     </span>
                                 <span style="margin-left: auto; margin-right: -20px; text-align: right;">
                     	<h4 style="font-weight: bold;">
-				  <i class="fas fa-eye"></i><?php echo $views ?></h4>
+				  <i class="fas fa-eye" style="margin-right: 5px;"></i><?php echo $views ?></h4>
                       <i class="fas fa-arrow-up" style="margin-right: 5px;"></i> 32 <i class="fas fa-arrow-down" style="margin-left: 10px; margin-right: 5px;"></i> 3
                     </span>
                             </div><br>
