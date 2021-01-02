6343932cf960a68f00593120f79887289a4fa6a7
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-01-02 23:30:06 +0100

    Update preset.php

--- preset.php
+++ preset.php
@@ -122,7 +122,7 @@ try {
 			echo $e2_err;
 		?>
 				<h3 style="font-weight: bold;">Resetowanie hasła do konta Viddle</h3>
-				<p>w celu zresetowania hasła, wyślemy na adres e-mail twojego konta e-maila z linkiem do resetowania hasła</p><br>
+				<p>Zapomniałeś hasła? Cóż, nawet najlepszym się to zdarza. Wpisz adres e-mail powiązany z Twoim kontem, żeby zresetować hasło.</p><br>
 				<?php
 
 
