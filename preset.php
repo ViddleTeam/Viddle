bbe04c0e6ee8b15a34e1506ffe430a32fde2f28c
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-01-02 23:05:01 +0100

    Update preset.php

--- preset.php
+++ preset.php
@@ -9,6 +9,7 @@ use PHPMailer\PHPMailer\Exception;
 
 // Load Composer's autoloader
 require 'vendor/autoload.php';
+require 'danemail.php';
 
 $e1_err = '';
 $e2_err = '';
@@ -55,30 +56,24 @@ $mail = new PHPMailer(true);
 
 
 
-    $mail->SMTPOptions = array(
-        'ssl' => array(
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true
-        )
-        );
+    
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->Host       = 'smtp.zoho.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'programiscidiscord@gmail.com';                     // SMTP username
-    $mail->Password   = 'l';                               // SMTP password
+    $mail->Username   = 'support@viddle.xyz';                     // SMTP username
+    $mail->Password   = 'b33@tbKz2fkTXxV';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;        // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                            // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->Port       = 465;                            // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('programiscidiscord@gmail.com', 'axel shop');
+    $mail->setFrom('support@viddle.xyz', 'viddle');
     $mail->addAddress($email);     // Add a recipient
     
     // Attachments
 
-    $body = 'Hej!!! oto jest kod weryfikacji adresu e-mail w naszym serwisie:'.$kod.'. Pozdrawiamy';
+    $body = 'test';
 
 
     // Content
