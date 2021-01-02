6d180d78237f83d4de9bc8eb8fb2a20d9a231328
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-01-02 23:24:42 +0100

    Update preset.php

--- preset.php
+++ preset.php
@@ -52,42 +52,33 @@ if (isset($_POST['email']))
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
-
-
-   
-    $mail->SMTPOptions = array(
-        'ssl' => array(
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true
-        )
-        );
-
-    
+try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.zoho.com';                    // Set the SMTP server to send through
+    $mail->Host       = 'smtppro.zoho.eu';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'support@viddle.xyz';                     // SMTP username
     $mail->Password   = 'b33@tbKz2fkTXxV';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;        // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 465;                            // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
     $mail->setFrom('support@viddle.xyz', 'viddle');
     $mail->addAddress($_POST['email']);     // Add a recipient
-    
-    // Attachments
-
-    $body = 'test';
-
-
+  
+  
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'WERYFIKACJA ADRESU E-MAIL';
-    $mail->Body    = $body;
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
     $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 	}
 }
 			
