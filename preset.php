21a158a4022ae2c633275aa5638fd5ef731bdd25
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-01-02 23:27:09 +0100

    Update preset.php

--- preset.php
+++ preset.php
@@ -54,7 +54,7 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->SMTPDebug = SMTP::DEBUG_OFF;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtppro.zoho.eu';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
