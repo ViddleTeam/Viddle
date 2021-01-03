fa4846106549936fe3e03a416453c214fd42bf52
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-01-03 19:58:59 +0100

    Update reset.php

--- reset.php
+++ reset.php
@@ -10,6 +10,27 @@ if ($result = @$connect->query(
 		    mysqli_real_escape_string($connect,$id))))
 
 $d2 = $result->num_rows;
-
+if ($d2 == '0') {
+	echo "<script>
+		$('#staticBackdrop').modal('show');
+	</script>";
+}
 ?>
 
+<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Wystąpił błąd!</h5>
+            </div>
+            <div class="modal-body">
+                Link do odzyskania hasła jest nieprawidłowy bądź jego ważność wygasła.
+            </div>
+            <div class="modal-footer">
+                <a href="index.php"><button type="button" class="btn btn-primary" style="padding: 10px;">Powrót na stronę główną</button></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
