ca247e2f2416448428dee2f32ee610e3ac1e6531
evoDesign <57808067+evolutionevotv@users.noreply.github.com>
2021-01-06 19:12:45 +0100

    Update navbar.php

--- partials/navbar.php
+++ partials/navbar.php
@@ -76,7 +76,7 @@ if($block == '1')
 <div style="opacity: 1;" class="website">
     <header>
         <nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar top-nav-collapse" style="height: fit-content; background-color: #212121;">
-            <a class="navbar-brand" href="index.php"><img src="https://media.discordapp.net/attachments/785086822220169217/796438597258444830/vlogocropped.png?width=20&height=20" width="20px" height="20px"/></a>
+            <a class="navbar-brand" href="index.php"><img src="https://media.discordapp.net/attachments/785086822220169217/796438597258444830/vlogocropped.png?width=25&height=25" width="25px" height="25px"/></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-6"
                     aria-controls="navbarSupportedContent-6" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
