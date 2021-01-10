cb821eab50f63d235e1d8cf665b53a3934872d33
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-01-10 15:22:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -125,10 +125,32 @@ if ($viewsthree > 999 && $viewsthree <= 999999) {
                   <h4 class="tile-before" style="color:white;">Ostatnio udostępnione filmy</h4>
               </div>
             </div>
+		  <?php 
+		  
+		  if ($resultn1 = @$connect->query(
+		    sprintf("SELECT * FROM viddle_videos WHERE video_id='%s'",
+		    mysqli_real_escape_string($connect,$oneid))))
+			
+				    $dn1 = $resultn1->num_rows;
+			    
+			    	    $danen1 = $resultn1->fetch_assoc();
+			    	    $avn1 = $danen1['minname'];
+			    
+			  	if($avn1 == 'x')
+				{
+					$avnf1 = 'https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg';
+				}
+			    	else
+				{
+					$avnf1 = '/grafic/'.$oneid.'m.'.$avn1.'';
+				}
+		       		
+		  
+		  ?>
             <div class="tile" style="margin: auto;">
                 <div class='card'>
                         <a href='video?id=<?php echo($oneid); ?>'>
-                        <img src='https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg' class='img-responsive card-img'>
+                        <img src='<?php echo $avnf1 ?>'>
                         <p class='card-title'><?php echo($titleone); ?></p>
                         <div class='hr' style='margin-top:-5px;margin-bottom:5px;'></div>
                         <div class='bottom-info'>
@@ -139,8 +161,29 @@ if ($viewsthree > 999 && $viewsthree <= 999999) {
                         </a>
                     </div>
                     <div class='card'>
+			<?php
+			    if ($resultn2 = @$connect->query(
+		    sprintf("SELECT * FROM viddle_videos WHERE video_id='%s'",
+		    mysqli_real_escape_string($connect,$twoid))))
+			
+				    $dn2 = $resultn2->num_rows;
+			    
+			    	    $danen2 = $resultn2->fetch_assoc();
+			    	    $avn2 = $danen2['minname'];
+			    
+			  	if($avn2 == 'x')
+				{
+					$avnf2 = 'https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg';
+				}
+			    	else
+				{
+					$avnf2 = '/grafic/'.$twoid.'m.'.$avn2.'';
+				}
+		       		
+				    
+			?>
                         <a href='video?id=<?php echo($twoid); ?>'>
-                        <img src='https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg' class='img-responsive card-img'>
+                        <img src='<?php echo $avnf2 ?>'>
                         <p class='card-title'><?php echo($titletwo); ?></p>
                         <div class='hr' style='margin-top:-5px;margin-bottom:5px;'></div>
                         <div class='bottom-info'>
