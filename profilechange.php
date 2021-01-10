4013704151d2f5079247aa60a75a445fa298411e
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-01-10 23:09:20 +0100

    Update profilechange.php

--- profilechange.php
+++ profilechange.php
@@ -88,7 +88,7 @@ require_once('partials/navbar.php');
 				<img width="204px" style="border-radius:50%; margin-bottom:5px;" class="img-responsive" src="<?php echo $av4 ?>"><br>
 		      	<button type="button" class="btn btn-gray" data-toggle="modal" data-target="#modalAvatar"><p style="margin: 10px;">Zmień awatar</p></button></a>
 				<button type="button" class="btn btn-blue-grey"><p style="margin: 10px;">Przywróć domyślny</p></button><br><br>
-				<p>Obsługiwane formaty: JPG, JPEG, PNG oraz BMP. Aktualizacja może potrwać do kilku minut. <?php echo $_FILES['file_picker']['extension'] ?></p>
+				<p>Obsługiwane formaty: JPG, JPEG, PNG oraz BMP. Aktualizacja może potrwać do kilku minut. <?php echo $_FILES['file_picker']['type']?></p>
 			      </div>
 		      </center>
 								
