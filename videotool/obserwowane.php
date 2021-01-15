37f7be4bf878c352860406cf76dfda71965b24a1
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-01-15 14:37:00 +0100

    Update obserwowane.php

--- videotool/obserwowane.php
+++ videotool/obserwowane.php
@@ -1,3 +1,37 @@
+<?php
+
+if(isset($_POST['ob']))
+{
+
+
+
+		$ok = true;
+
+		$ahaha = $dane['observators'] + 1;
+
+	 if ($connect->query(
+				  sprintf("INSERT INTO viddle_obserwators VALUES (NULL, '%s', '%s', '%s', '%s', '%s'",
+				  mysqli_real_escape_string($connect,$ahaha),
+				  mysqli_real_escape_string($connect,$_SESSION['uid']),
+				  mysqli_real_escape_string($connect,$publisher),
+				  mysqli_real_escape_string($connect,'1'))))
+	    {
+		    if ($result = @$connect->query(
+		    sprintf("UPDATE `viddle_users` SET `observators`='%s' WHERE `uid`='%s'",
+		    mysqli_real_escape_string($connect,$ahaha),
+		    mysqli_real_escape_string($connect,$publisher))))
+		    {
+			    // dodano do obserwowanych
+			    
+			    
+		    }
+	    }
+
+
+
+}
+
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
