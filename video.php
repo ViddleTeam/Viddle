2df3a95bc5dff5159a2ca848521fbb59ec3bcd50
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-01-15 12:26:04 +0100

    Update video.php

--- video.php
+++ video.php
@@ -239,12 +239,12 @@ $_SESSION['id'] = $id;
 // 					
 					<?php if($obm == '1') { ?>
 					<form method="post">
-                    <input type="submit" name="ob" <?php echo $obd ?> value="Obserwuj' />
+                    <input type="submit" name="ob" <?php echo $obd ?> value="Obserwuj" />
 					</form>
 							  <?php } else { ?>
 							  
 							  	<form method="post">
-                    <input type="submit" name="unob" value="Obserwujujesz' />
+                    <input type="submit" name="unob" value="Obserwujujesz" />
 					</form>
 							  
 							  <?php } ?>
