4e5a9034338c20cc399a2703d566531c3bd5598f
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-01-21 10:08:35 +0100

    Create vinfo.php
    info o wysłaniu linka do weryfikacji maila

---
+++ vinfo.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+
+$id = $_GET['id'];
+
+require "danesql.php";
+$connect = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME);
+
+if ($result = @$connect->query(
+	sprintf("SELECT * FROM viddle_ver WHERE vid2='%s'",
+	mysqli_real_escape_string($connect,$id))))
+  {
+    $d2 = $result->num_rows;
+    
+    if($d2 == '0')
+    {
+      header('location: index.php');
+    }
+  }
+
+
+
+
+?>
+
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+	<style>
+	
+	</style>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+		<link rel="stylesheet" href="http://midacss.ml/assets/master.min.css" />
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Konto zostało założone.</title>
+		<link rel="stylesheet" href="https://cdn.patryqhyper.pl/vdp/mdb/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://cdn.patryqhyper.pl/vdp/mdb/css/mdb.min.css">
+		<link rel="stylesheet" href="style.css">
+		<meta property="og:title" content="Viddle">
+		<meta property="og:description" content="Filmy, muzyka i wiele więcej. Udostępniaj swoje filmy znajomym, rodzinie, oraz całemu światu, za pomocą VDP.">
+		<script src="script.js"></script>
+		<script src="https://kit.fontawesome.com/604acb9c5e.js"></script>
+		<link rel="stylesheet" href="https://kit-free.fontawesome.com/releases/latest/css/free.min.css" media="all"><link rel="stylesheet" href="https://kit-free.fontawesome.com/releases/latest/css/free-v4-font-face.min.css" media="all"><link rel="stylesheet" href="https://kit-free.fontawesome.com/releases/latest/css/free-v4-shims.min.css" media="all">
+	</head>
+	<body class="bg-img img-fluid" style="align-items: center; justify-content: center; text-align: center;">
+			<div class="card-login" style="width: 550px; height: auto; align-items: center; padding: 15px 0 15px 0;">
+				<div class="card-body">
+                                <div id="main">
+				<h3 style="font-weight: bold;">Weryfikacja adresu e-mail</h3>
+				<p>Na adres e-mail <?php echo $_SESSION['email'] ?> wysłaliśmy link do weryfikacji adresu e-mail. Sprawdź swoją skrzynke e-mail i kliknij w link wysłany przez nas.</p><br>
+				
+                
+					<a href="index.php"><button type="button" class="btn btn-blue-grey"><p style="margin: 10px;">Strona główna</p></button></a>
+				</div>
+				</div>
+				</center>
+			</div>
+			</div><br>
+	</body>
+</html>
