3f7a8a67010ad0f45038a804b104411d48d60b42
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-01-21 09:02:32 +0100

    Create danemail.php

---
+++ danemail.php
@@ -0,0 +1,9 @@
+<?php
+
+define('MAILUSER', 'support@viddle.xyz');
+define('MAILPASS', '3fpbmx@b6oSw9kyu');
+define('MAILHOST', 'smtppro.zoho.eu');
+define('MAILPORT', '465'); 
+define('MAILNAME', 'viddle');
+
+?>
