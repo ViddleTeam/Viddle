4faf05262af0b534a243300184e211fde56ec6a8
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-01-23 19:48:38 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -19,9 +19,6 @@ require_once ('partials/navbar.php');
         <div class="col-lg-12">
             <h4 class="tile-before" style="color:white; margin-top: 40px;"><br>Udostępnij film na Viddle</h4>
             <p style="color: white; margin-bottom: 20px;">Krok 1/3: Wybierz film do udostępnienia</p>
-            <div class="alert alert-warning">
-                Uwaga: ze względu na limity hostingu, limit jednego filmu na Viddle obecnie wynosi 10 MB. <a class="alert-link" href="https://support.infinityfree.net/files/how-to-upload-big-files-archives/">Dlaczego?</a>
-            </div>
         </div>
     </div>
     <div class="tile" style="margin: auto;">
@@ -30,7 +27,7 @@ require_once ('partials/navbar.php');
                 <p style="align-items: center; color: white;">
                     <center>
                         <h2 style="color: white;">Wybierz plik wideo z komputera/telefonu</h2>
-                <p style="color: white;">Maksymalny dozwolony rozmiar na jeden film wynosi 10 MB. Chcesz się dowiedzieć, ile dany film może ważyć? Użyj <a href="https://toolstud.io/video/filesize.php?">tego narzędzia.</a></p>
+                <p style="color: white;">Maksymalny dozwolony rozmiar na jeden film wynosi 5 GB. Chcesz się dowiedzieć, ile dany film może ważyć? Użyj <a href="https://toolstud.io/video/filesize.php?">tego narzędzia.</a></p>
                 <p style="color: white; font-weight: bold; margin-top: -10px;">Pamiętaj, że administracja Viddle ma pełne prawo do usunięcia filmu, jeżeli narusza on prawa autorskie i/lub regulamin.</p>
                     <form action="/post.php" method="post" enctype="multipart/form-data">
                     <input type="file" accept="video/mp4, video/mov, video/webm, video/x-ms-wmv, video/3gpp" name="videovid" style="color: white; margin-top: 5px;" />
