ca1389cae3b655b697a9fef2fe0a650f9a4fa1c7
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-01-23 19:45:44 +0100

    Update channel.php

--- channel.php
+++ channel.php
@@ -69,7 +69,7 @@ if ($result = @$connect->query(
 				<span style="margin-left: 10px; margin-bottom: 10px;">
 					<img width="64px" style="border-radius:50%;margin-right:5px;" class="img-responsive" src="<?php if (isset($av4)) echo $av4; ?>">
 				</span>
-				<span style="text-align: left; margin-left: 10px; margin-right: auto;">
+				<span style="margin-left: 10px; margin-right: auto;">
 					<h3><?php echo $nazwa; ?></h3>
 					<p style="text-align: left; margin-bottom: 20px;"><?php if (isset($obserwatorzy)) echo $obserwatorzy; ?> obserwujących</p>
 				</span>
@@ -93,128 +93,6 @@ if ($result = @$connect->query(
 				
 			</div>
 			<div class="container" style="margin-top:20px;">
-			<ul class="nav nav-tabs" id="myTab" role="tablist">
-			  <li class="nav-item">
-			    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Strona główna</a>
-			  </li>
-			  <li class="nav-item">
-			    <a class="nav-link" id="videos-tab" data-toggle="tab" href="#videos" role="tab" aria-controls="videos" aria-selected="false">Filmy</a>
-			  </li>
-			</ul>
-			<div class="tab-content" id="myTabContent">
-			  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-				<div class="row">
-				  <div class="col-lg-12" style="display: -webkit-box;display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;flex-direction: column;-webkit-box-align: center;align-items: center;-webkit-box-pack: center;justify-content: center;">
-				</div>
-				  <div class="row">
-				    <h4 class="tile-before" style="color:white; margin-left: 10px;">Ostatnio udostępnione</h4>
-				  </div><br>
-					<div class="container row" style="min-width: 100%">
-							<?php
-							if ($result3 = @$connect->query(
-					    sprintf("SELECT * FROM viddle_videos WHERE publisher='%s'",
-					    mysqli_real_escape_string($connect,$id))))	
-					    $d4 = $result->num_rows;
-
-					    if(!$d4 == '0') {
-						    $p1 = $connect->query("SELECT * FROM viddle_videos WHERE publisher='$id'");
-					    if($p1->num_rows > 0){
-						$num = $p1->num_rows+1;
-						for($k1 = 1; $k1 < 4; $k1 += 1){
-						    if($k2 = $p1 = $connect->query("SELECT * FROM viddle_videos WHERE il='$k1' AND publisher='$id'")){
-							$d5 = $k2->fetch_assoc();
-							    if($d5['minname'] == 'x') {
-								$miniaturka = 'https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg';
-								   } else {
-									   $miniaturka = 'grafic/'.$d5['video_id'].'m.'.$d5['minname'].'';
-								   }
-							echo '
-							<div class="card">
-								<a href="video?id='.$d5['video_id'].'">
-								<img src="'.$miniaturka.'" class="img-responsive card-img" width="300" height="187.5">
-								<p class="card-title">'.$d5['title'].'</p>
-								<div class="hr" style="margin-top:-5px;margin-bottom:5px;"></div>
-								<div class="bottom-info">
-									<span>'.$nazwa.'</span>
-									<span>•</span>
-									<span>'.$d5['views'].' wyświetleń</span>
-								</div>
-								</a>
-							</div>';
-						}
-					    }
-					    }
-					    } else {
-						    if (!$do == 0) {
-										echo '<div class="alert alert-info" style="width: 100%;">
-												<strong>Trochę tu pusto.</strong> Dodaj swój pierwszy film poprzez kliknięcie ikony tuż obok menu konta.
-											  </div>';
-									} else {
-											echo '<div class="alert alert-info" style="width: 100%;">
-													<strong>'.$nazwa.' nie posiada żadnych filmów.</strong>
-												  </div>';
-									}
-					    }	
-									?>
-					</div>
-				</div>
-			      </div>
-				</div>
-					</center>
-					</div>	
-			  </div>
-			  <div class="tab-pane fade" id="videos" role="tabpanel" aria-labelledby="videos-tab">
-				  <h3>Wszystkie filmy na kanale</h3>
-				  <div class="container row" style="min-width: 100%">
-							<?php
-							if ($result3 = @$connect->query(
-					    sprintf("SELECT * FROM viddle_videos WHERE publisher='%s'",
-					    mysqli_real_escape_string($connect,$id))))	
-					    $d4 = $result->num_rows;
-
-					    if(!$d4 == '0') {
-						    $p1 = $connect->query("SELECT * FROM viddle_videos WHERE publisher='$id'");
-					    if($p1->num_rows > 0){
-						$num = $p1->num_rows+1;
-						for($k1 = 1; $k1 < $num; $k1 += 1){
-						    if($k2 = $p1 = $connect->query("SELECT * FROM viddle_videos WHERE il='$k1' AND publisher='$id'")){
-							$d5 = $k2->fetch_assoc();
-							    if($d5['minname'] == 'x') {
-								$miniaturka = 'https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg';
-								   } else {
-									   $miniaturka = 'grafic/'.$d5['video_id'].'m.'.$d5['minname'].'';
-								   }
-							echo '
-							<div class="card">
-								<a href="video?id='.$d5['video_id'].'">
-								<img src="'.$miniaturka.'" class="img-responsive card-img" width="300" height="187.5">
-								<p class="card-title">'.$d5['title'].'</p>
-								<div class="hr" style="margin-top:-5px;margin-bottom:5px;"></div>
-								<div class="bottom-info">
-									<span>'.$nazwa.'</span>
-									<span>•</span>
-									<span>'.$d5['views'].' wyświetleń</span>
-								</div>
-								</a>
-							</div>';
-						}
-					    }
-					    }
-					    } else {
-						    if (!$do == 0) {
-										echo '<div class="alert alert-info" style="width: 100%;">
-												<strong>Trochę tu pusto.</strong> Dodaj swój pierwszy film poprzez kliknięcie ikony tuż obok menu konta.
-											  </div>';
-									} else {
-											echo '<div class="alert alert-info" style="width: 100%;">
-													<strong>'.$nazwa.' nie posiada żadnych filmów.</strong>
-												  </div>';
-									}
-					    }	
-									?>
-					</div>
-			  </div>
-			</div>	
         <div class="row">
           <div class="col-lg-12" style="display: -webkit-box;display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;flex-direction: column;-webkit-box-align: center;align-items: center;-webkit-box-pack: center;justify-content: center;">
         </div>
@@ -224,23 +102,32 @@ if ($result = @$connect->query(
           </div><br>
 		<div class="container row" style="min-width: 100%">
 				<?php
+				
 				if ($result3 = @$connect->query(
 		    sprintf("SELECT * FROM viddle_videos WHERE publisher='%s'",
-		    mysqli_real_escape_string($connect,$id))))	
+		    mysqli_real_escape_string($connect,$id))))
+					
 		    $d4 = $result->num_rows;
 				
-		    if(!$d4 == '0') {
+		    if(!$d4 == '0')
+		    {
 			    $p1 = $connect->query("SELECT * FROM viddle_videos WHERE publisher='$id'");
+                    
                     if($p1->num_rows > 0){
                         $num = $p1->num_rows+1;
-                        for($k1 = 1; $k1 < 4; $k1 += 1){
+                        for($k1 = 1; $k1 < $num; $k1 += 1){
                             if($k2 = $p1 = $connect->query("SELECT * FROM viddle_videos WHERE il='$k1' AND publisher='$id'")){
                                 $d5 = $k2->fetch_assoc();
-				    if($d5['minname'] == 'x') {
+				    
+				    if($d5['minname'] == 'x')
+				    {
 				    	$miniaturka = 'https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg';
-					   } else {
+					   }
+					   else
+					   {
 						   $miniaturka = 'grafic/'.$d5['video_id'].'m.'.$d5['minname'].'';
 					   }
+
                                 echo '
                                 <div class="card">
 					<a href="video?id='.$d5['video_id'].'">
@@ -254,11 +141,13 @@ if ($result = @$connect->query(
 					</div>
 					</a>
 				</div>';
+                                
+                            
                         }
                     }
 		    }
 		    } else {
-			    if (!$do == 0) {
+			    if ($do == 1) {
 							echo '<div class="alert alert-info" style="width: 100%;">
 									<strong>Trochę tu pusto.</strong> Dodaj swój pierwszy film poprzez kliknięcie ikony tuż obok menu konta.
 								  </div>';
