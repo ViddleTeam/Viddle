0fe70e1df72227dc7a38a22e901474909b99f620
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-01-28 11:35:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -66,6 +66,17 @@ if ($viewsthree > 999 && $viewsthree <= 999999) {
     $rezultattrzy = $viewsthree;
 }
 ?>
+<head>
+	<script>
+		$(document).ready(function(){
+    if($("#notify").width() > 0) {
+
+    } else {
+        alert('AdBlocker Detected');
+    }
+});
+	</script>
+</head>
       <div class="container" style="margin-top:30px;">
         <div class="row">
           <div class="col-lg-12" style="display: -webkit-box;display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;flex-direction: column;-webkit-box-align: center;align-items: center;-webkit-box-pack: center;justify-content: center;">
