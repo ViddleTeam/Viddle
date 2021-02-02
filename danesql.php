8a21ae67cec70a8bd9d684a5e0ee5a2edce561be
Lort533 <76491869+Lort533@users.noreply.github.com>
2021-02-02 22:58:43 +0100

    Update danesql.php

--- danesql.php
+++ danesql.php
@@ -1,5 +1,5 @@
 <?php
 define('SQLHOST', 'localhost');
-define('SQLPASS', '$dB2?Km+5ujf]:E%J>7C:2');
+define('SQLPASS', 'zLjOJcX2sjpqaXqq');
 define('SQLUSER', 'viddle');
 define('DBNAME', 'viddle');
