6521d3442337964a3b034bacc348e25e99190f72
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-02-04 23:27:20 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,24 @@
 <?php
-$ip = $_SERVER['HTTP_CLIENT_IP'];
+function get_ip($ip2long = true)
+{
+ if($_SERVER['HTTP_CLIENT_IP'])
+ {
+  $ip = $_SERVER['HTTP_CLIENT_IP'];
+ }
+ else if($_SERVER['HTTP_X_FORWARDED_FOR'])
+ {
+  $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+ }
+ else
+ {
+  $ip = $_SERVER['REMOTE_ADDR'];
+ }
 
-echo $ip;
+ if($ip2long)
+ {
+  $ip = ip2long($ip);
+ }
+
+ return $ip;
+}
 ?>
