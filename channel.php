d1bbd313cf3ae56e679fe83df23d21c1493ded11
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-02-04 17:00:39 +0100

    Update channel.php

--- channel.php
+++ channel.php
@@ -47,7 +47,7 @@ if ($result = @$connect->query(
 		<div class="banner mx-auto">
 			<img class="img-fluid" src="<?php if (isset($ba3)) echo $ba3; ?>" style="margin: auto; " height="100%" width="100%"/>
 		</div>
-			<div class="card-channel" style="height: auto; width: 100%; margin-top: -30px; margin-left: 0px; margin-right: 0px;">
+			<div class="card-channel" style="height: 100px; width: 100%; margin-top: -30px; margin-left: 0px; margin-right: 0px;">
 				<div class="card-body row" style="color: white;">
 				<span style="margin-left: 10px; margin-bottom: 10px;">
 					<img width="64px" style="border-radius:50%;margin-right:5px;" class="img-responsive" src="<?php if (isset($av4)) echo $av4; ?>">
@@ -72,154 +72,80 @@ if ($result = @$connect->query(
 					</div>
 				</span>
 				</div>
+				
+				
 			</div>
-			<div class="bottom-radius" style="height: 100px; width: 100%; margin-left: 0px; margin-right: 0px; margin-top: -30px;">
-					<ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
-						<li class="nav-item">
-							<a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home"
-							aria-selected="true">Strona główna</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" id="videos-tab" data-toggle="tab" href="#videos" role="tab" aria-controls="profile"
-							aria-selected="false">Filmy</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" id="channels-tab" data-toggle="tab" href="#channels" role="tab" aria-controls="contact"
-							aria-selected="false">Kanały</a>
-						</li>
-					</ul>
-				</div>
-		<div class="container" style="margin-top:20px;">
-			<div class="row">
-				<div class="col-lg-12" style="display: -webkit-box;display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;flex-direction: column;-webkit-box-align: center;align-items: center;-webkit-box-pack: center;justify-content: center;"></div>
-			</div>
-			<div class="container row" style="min-width: 100% !important;">
-			 <div class="tab-content" id="myTabContent" style="margin-top: -20px;">
-			 	<div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-				  <div class="row">
-					  <h4 class="tile-before" style="color:white; margin-left: 10px;">Ostatnio udostępnione</h4>
-				  </div><br>
-				  <div class="container row">
-					<?php
-					if ($result3 = @$connect->query(sprintf("SELECT * FROM viddle_videos WHERE publisher='%s'", mysqli_real_escape_string($connect,$id))))
-					$d4 = $result->num_rows;
-					if (!$d4 == '0') {
-						$p1 = $connect->query("SELECT * FROM viddle_videos WHERE `publisher`='$id'");
-							if($p1->num_rows > 0){
-								$num = $p1->num_rows;
-								for($k1 > 3; $k1; $k1 += 1){
-									if($k2 = $p1 = $connect->query("SELECT * FROM viddle_videos WHERE il='$k1' AND publisher='$id'")){
-										$d5 = $k2->fetch_assoc();
-							if($d5['minname'] == 'x') {
-								$miniaturka = 'https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg';
-							} else {
-								$miniaturka = 'grafic/'.$d5['video_id'].'m.'.$d5['minname'].'';
-							}
-							echo '
-										<div class="card">
-							<a href="video?id='.$d5['video_id'].'">
-							<img src="'.$miniaturka.'" class="img-responsive card-img" width="300" height="187.5">
-							<p class="card-title">'.$d5['title'].'</p>
-							<div class="hr" style="margin-top:-5px;margin-bottom:5px;"></div>
-							<div class="bottom-info">
-								<span>'.$nazwa.'</span>
-								<span>•</span>
-								<span>'.$d5['views'].' wyświetleń</span>
-							</div>
-							</a>
-						</div>';  
-								}
-							}
-					}
-					} else {
-						if ($do == 1) {
-									echo '<div class="alert alert-info" style="width: 100%;">
-											<strong>Trochę tu pusto.</strong> Dodaj swój pierwszy film poprzez kliknięcie ikony tuż obok menu konta.
-										</div>';
-								} else {
-										echo '<div class="alert alert-info" style="width: 100%;">
-												<strong>'.$nazwa.' nie posiada żadnych filmów.</strong>
-											</div>';
-								}
-					}	
-					?>
+			<div class="container" style="margin-top:20px;">
+        <div class="row">
+          <div class="col-lg-12" style="display: -webkit-box;display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;flex-direction: column;-webkit-box-align: center;align-items: center;-webkit-box-pack: center;justify-content: center;">
+        </div>
+        </div>
+          <div class="row">
+            <h4 class="tile-before" style="color:white; margin-left: 10px;">Ostatnio udostępnione</h4>
+          </div><br>
+		<div class="container row" style="min-width: 100%">
+				<?php
+				
+				if ($result3 = @$connect->query(
+		    sprintf("SELECT * FROM viddle_videos WHERE publisher='%s'",
+		    mysqli_real_escape_string($connect,$id))))
+					
+		    $d4 = $result->num_rows;
+				
+		    if(!$d4 == '0')
+		    {
+			    $p1 = $connect->query("SELECT * FROM viddle_videos WHERE `publisher`='$id'");
+			    
+                    
+                    if($p1->num_rows > 0){
+                        $num = $p1->num_rows;
+                        for($k1 = $num; $k1; $k1 -= 1){
+                            if($k2 = $p1 = $connect->query("SELECT * FROM viddle_videos WHERE il='$k1' AND publisher='$id'")){
+                                $d5 = $k2->fetch_assoc();
+				 
+				    
+				 
+				    if($d5['minname'] == 'x')
+				    {
+				    	$miniaturka = 'https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg';
+					   }
+					   else
+					   {
+						   $miniaturka = 'grafic/'.$d5['video_id'].'m.'.$d5['minname'].'';
+					   }
+
+                                echo '
+                                <div class="card">
+					<a href="video?id='.$d5['video_id'].'">
+					<img src="'.$miniaturka.'" class="img-responsive card-img" width="300" height="187.5">
+					<p class="card-title">'.$d5['title'].'</p>
+					<div class="hr" style="margin-top:-5px;margin-bottom:5px;"></div>
+					<div class="bottom-info">
+						<span>'.$nazwa.'</span>
+						<span>•</span>
+						<span>'.$d5['views'].' wyświetleń</span>
 					</div>
-					</div>
-					<div class="tab-pane fade" id="videos" role="tabpanel" aria-labelledby="videos-tab">
-						<div class="container row" style="min-width: 100%;">
-							<h4 class="tile-before" style="color:white; margin-left: 10px;">Wszystkie filmy</h4>
-							<?php
-								if ($result3 = @$connect->query(sprintf("SELECT * FROM viddle_videos WHERE publisher='%s'", mysqli_real_escape_string($connect,$id))))
-								$d4 = $result->num_rows;
-								if (!$d4 == '0') {
-									$p1 = $connect->query("SELECT * FROM viddle_videos WHERE `publisher`='$id'");
-										if($p1->num_rows > 0){
-											$num = $p1->num_rows;
-											for($k1 = $num; $k1; $k1 -= 1){
-												if($k2 = $p1 = $connect->query("SELECT * FROM viddle_videos WHERE il='$k1' AND publisher='$id'")){
-													$d5 = $k2->fetch_assoc();
-										if($d5['minname'] == 'x') {
-											$miniaturka = 'https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg';
-										} else {
-											$miniaturka = 'grafic/'.$d5['video_id'].'m.'.$d5['minname'].'';
-										}
-										echo '
-													<div class="card">
-										<a href="video?id='.$d5['video_id'].'">
-										<img src="'.$miniaturka.'" class="img-responsive card-img" width="300" height="187.5">
-										<p class="card-title">'.$d5['title'].'</p>
-										<div class="hr" style="margin-top:-5px;margin-bottom:5px;"></div>
-										<div class="bottom-info">
-											<span>'.$nazwa.'</span>
-											<span>•</span>
-											<span>'.$d5['views'].' wyświetleń</span>
-										</div>
-										</a>
-									</div>';  
-											}
-										}
-								}
-								} else {
-									if ($do == 1) {
-												echo '<div class="alert alert-info" style="width: 100%;">
-														<strong>Trochę tu pusto.</strong> Dodaj swój pierwszy film poprzez kliknięcie ikony tuż obok menu konta.
-													</div>';
-											} else {
-													echo '<div class="alert alert-info" style="width: 100%;">
-															<strong>'.$nazwa.' nie posiada żadnych filmów.</strong>
-														</div>';
-											}
-								}	
-							?>
-						</div>
-					</div>
-					<div class="tab-pane fade" id="channels" role="tabpanel" aria-labelledby="channels-tab">
-						<h4 class="tile-before" style="color:white; margin-left: 10px;">Obserwowane kanały</h4>
-						<div class="container row" id="channelsContainer">
-							<span class="followedChannel">
-								<img src="https://cdn.ampersandbot.pl/bOziN3jpd.jpg" id="followedChannelAvatar" width="96px" height="96px" /><br>
-								<p class="followedChannelName">Nazwa kanału</p>
-							</span>
-							<span class="followedChannel">
-								<img src="https://cdn.ampersandbot.pl/bOziN3jpd.jpg" id="followedChannelAvatar" width="96px" height="96px" /><br>
-								<p class="followedChannelName">Nazwa kanału</p>
-							</span>
-							<span class="followedChannel">
-								<img src="https://cdn.ampersandbot.pl/bOziN3jpd.jpg" id="followedChannelAvatar" width="96px" height="96px" /><br>
-								<p class="followedChannelName">Nazwa kanału</p>
-							</span>
-							<span class="followedChannel">
-								<img src="https://cdn.ampersandbot.pl/bOziN3jpd.jpg" id="followedChannelAvatar" width="96px" height="96px" /><br>
-								<p class="followedChannelName">Nazwa kanału</p>
-							</span>
-							<span class="followedChannel">
-								<img src="https://cdn.ampersandbot.pl/bOziN3jpd.jpg" id="followedChannelAvatar" width="96px" height="96px" /><br>
-								<p class="followedChannelName">Nazwa kanału</p>
-							</span>
-						</div>
-					</div>
-				</div>
-			</div>
+					</a>
+				</div>';
+                                
+                            
+                        }
+			    
+                    }
+		    }
+		    } else {
+			    if ($do == 1) {
+							echo '<div class="alert alert-info" style="width: 100%;">
+									<strong>Trochę tu pusto.</strong> Dodaj swój pierwszy film poprzez kliknięcie ikony tuż obok menu konta.
+								  </div>';
+						} else {
+								echo '<div class="alert alert-info" style="width: 100%;">
+									  	<strong>'.$nazwa.' nie posiada żadnych filmów.</strong>
+									  </div>';
+						}
+		    }	
+						?>
+		</div>
 	    <!--
 	    // tutaj placeholdery do wyświetlania filmów
             // coś tutaj jeszcze sie zmieni, zobaczysz - evo
@@ -287,7 +213,6 @@ if ($result = @$connect->query(
     </div>
   </div>
 </div>
-
 <?php 
 require_once('partials/footer.php');
 ?>
