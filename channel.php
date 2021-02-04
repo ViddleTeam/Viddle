f93ac2e26f14ccce634e571b8a58d96adb013827
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-02-04 16:23:41 +0100

    Karty na kanale oraz usunięcie pustych linijek
    karty - m.in. strona główna, filmy czy kanały

--- channel.php
+++ channel.php
@@ -72,80 +72,119 @@ if ($result = @$connect->query(
 					</div>
 				</span>
 				</div>
-				
-				
+				<div class="bottom-radius" style="height: 100px; width: 100%; margin-top: -30px; margin-left: 0px; margin-right: 0px;">
+					<ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
+						<li class="nav-item">
+							<a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home"
+							aria-selected="true">Strona główna</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile"
+							aria-selected="false">Filmy</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact"
+							aria-selected="false">Kanały</a>
+						</li>
+					</ul>
+				</div>
 			</div>
-			<div class="container" style="margin-top:20px;">
-        <div class="row">
-          <div class="col-lg-12" style="display: -webkit-box;display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;flex-direction: column;-webkit-box-align: center;align-items: center;-webkit-box-pack: center;justify-content: center;">
-        </div>
-        </div>
-          <div class="row">
-            <h4 class="tile-before" style="color:white; margin-left: 10px;">Ostatnio udostępnione</h4>
-          </div><br>
-		<div class="container row" style="min-width: 100%">
-				<?php
-				
-				if ($result3 = @$connect->query(
-		    sprintf("SELECT * FROM viddle_videos WHERE publisher='%s'",
-		    mysqli_real_escape_string($connect,$id))))
-					
-		    $d4 = $result->num_rows;
-				
-		    if(!$d4 == '0')
-		    {
-			    $p1 = $connect->query("SELECT * FROM viddle_videos WHERE `publisher`='$id'");
-			    
-                    
-                    if($p1->num_rows > 0){
-                        $num = $p1->num_rows;
-                        for($k1 = $num; $k1; $k1 -= 1){
-                            if($k2 = $p1 = $connect->query("SELECT * FROM viddle_videos WHERE il='$k1' AND publisher='$id'")){
-                                $d5 = $k2->fetch_assoc();
-				 
-				    
-				 
-				    if($d5['minname'] == 'x')
-				    {
-				    	$miniaturka = 'https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg';
-					   }
-					   else
-					   {
-						   $miniaturka = 'grafic/'.$d5['video_id'].'m.'.$d5['minname'].'';
-					   }
-
-                                echo '
-                                <div class="card">
-					<a href="video?id='.$d5['video_id'].'">
-					<img src="'.$miniaturka.'" class="img-responsive card-img" width="300" height="187.5">
-					<p class="card-title">'.$d5['title'].'</p>
-					<div class="hr" style="margin-top:-5px;margin-bottom:5px;"></div>
-					<div class="bottom-info">
-						<span>'.$nazwa.'</span>
-						<span>•</span>
-						<span>'.$d5['views'].' wyświetleń</span>
+		<div class="container" style="margin-top:20px;">
+			<div class="row">
+				<div class="col-lg-12" style="display: -webkit-box;display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;flex-direction: column;-webkit-box-align: center;align-items: center;-webkit-box-pack: center;justify-content: center;"></div>
+			</div>
+			<div class="row">
+				<h4 class="tile-before" style="color:white; margin-left: 10px;">Ostatnio udostępnione</h4>
+			</div><br>
+			<div class="container row" style="min-width: 100%">
+			<!--
+				<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">Raw denim you
+    probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master
+    cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro
+    keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip
+    placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi
+    qui.</div>
+  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">Food truck fixie
+    locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit,
+    blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee.
+    Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum
+    PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS
+    salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit,
+    sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester
+    stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</div>
+  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">Etsy mixtape
+    wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack
+    lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard
+    locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify
+    squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel fixie
+    etsy retro mlkshk vice blog. Scenester cred you probably haven't heard of them, vinyl craft beer blog
+    stumptown. Pitchfork sustainable tofu synth chambray yr.</div>
+</div>
+			 -->
+			 <div class="tab-content" id="myTabContent">
+			 	<div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+					<?php
+					if ($result3 = @$connect->query(sprintf("SELECT * FROM viddle_videos WHERE publisher='%s'", mysqli_real_escape_string($connect,$id))))
+					$d4 = $result->num_rows;
+					if (!$d4 == '0') {
+						$p1 = $connect->query("SELECT * FROM viddle_videos WHERE `publisher`='$id'");
+							if($p1->num_rows > 0){
+								$num = $p1->num_rows;
+								for($k1 = $num; $k1; $k1 -= 1){
+									if($k2 = $p1 = $connect->query("SELECT * FROM viddle_videos WHERE il='$k1' AND publisher='$id'")){
+										$d5 = $k2->fetch_assoc();
+							if($d5['minname'] == 'x') {
+								$miniaturka = 'https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg';
+							} else {
+								$miniaturka = 'grafic/'.$d5['video_id'].'m.'.$d5['minname'].'';
+							}
+							echo '
+										<div class="card">
+							<a href="video?id='.$d5['video_id'].'">
+							<img src="'.$miniaturka.'" class="img-responsive card-img" width="300" height="187.5">
+							<p class="card-title">'.$d5['title'].'</p>
+							<div class="hr" style="margin-top:-5px;margin-bottom:5px;"></div>
+							<div class="bottom-info">
+								<span>'.$nazwa.'</span>
+								<span>•</span>
+								<span>'.$d5['views'].' wyświetleń</span>
+							</div>
+							</a>
+						</div>';  
+								}
+							}
+					}
+					} else {
+						if ($do == 1) {
+									echo '<div class="alert alert-info" style="width: 100%;">
+											<strong>Trochę tu pusto.</strong> Dodaj swój pierwszy film poprzez kliknięcie ikony tuż obok menu konta.
+										</div>';
+								} else {
+										echo '<div class="alert alert-info" style="width: 100%;">
+												<strong>'.$nazwa.' nie posiada żadnych filmów.</strong>
+											</div>';
+								}
+					}	
+					?>
 					</div>
-					</a>
-				</div>';
-                                
-                            
-                        }
-			    
-                    }
-		    }
-		    } else {
-			    if ($do == 1) {
-							echo '<div class="alert alert-info" style="width: 100%;">
-									<strong>Trochę tu pusto.</strong> Dodaj swój pierwszy film poprzez kliknięcie ikony tuż obok menu konta.
-								  </div>';
-						} else {
-								echo '<div class="alert alert-info" style="width: 100%;">
-									  	<strong>'.$nazwa.' nie posiada żadnych filmów.</strong>
-									  </div>';
-						}
-		    }	
-						?>
-		</div>
+					<div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">Food truck fixie
+						locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit,
+						blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee.
+						Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum
+						PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS
+						salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit,
+						sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester
+						stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</div>
+					<div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">Etsy mixtape
+						wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack
+						lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard
+						locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify
+						squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel fixie
+						etsy retro mlkshk vice blog. Scenester cred you probably haven't heard of them, vinyl craft beer blog
+						stumptown. Pitchfork sustainable tofu synth chambray yr.</div>
+				</div>
+			</div>
 	    <!--
 	    // tutaj placeholdery do wyświetlania filmów
             // coś tutaj jeszcze sie zmieni, zobaczysz - evo
