df856e627b1c6ea3ff6546746971ded3303d15e6
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-02-04 23:29:17 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,24 +1,15 @@
 <?php
-function get_ip($ip2long = true)
-{
- if($_SERVER['HTTP_CLIENT_IP'])
- {
-  $ip = $_SERVER['HTTP_CLIENT_IP'];
- }
- else if($_SERVER['HTTP_X_FORWARDED_FOR'])
- {
-  $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
- }
- else
- {
-  $ip = $_SERVER['REMOTE_ADDR'];
- }
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
 
- if($ip2long)
- {
-  $ip = ip2long($ip);
- }
-
- return $ip;
+if($ip == '') {
+ echo 'blad';
+} else {
+ echo $ip
 }
 ?>
