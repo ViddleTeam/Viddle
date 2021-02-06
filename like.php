fcfa619c65114d716999d5522c7f0f61c2472d46
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-02-06 22:13:10 +0100

    Update like.php

--- like.php
+++ like.php
@@ -64,10 +64,10 @@ if($d2 == '1')
 			$w = $dislike - '1';
 			$w2 = $like + '1';
 			if($gg = @$connect->query(
-			sprintf("DELETE FROM viddle_oceny WHERE videoid='%s' AND uid='%s' AND ocena='%s'",
+			sprintf("UPDATE viddle_videos SET ocena='%s' WHERE video_id='%s' AND user='%s'",
+			mysqli_real_escape_string($connect,$r),
 			mysqli_real_escape_string($connect,$id),
-			mysqli_real_escape_string($connect,$uid),
-			mysqli_real_escape_string($connect,$o)))) {
+			mysqli_real_escape_string($connect,$uid)))) {
 				echo '2';
 				
 				if($gg = @$connect->query(
