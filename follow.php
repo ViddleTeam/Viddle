ce3491c963e448d69ef5a42eb3248a1ba8070152
Lort533 <76491869+Lort533@users.noreply.github.com>
2021-02-08 14:38:03 +0100

    Update follow.php

--- follow.php
+++ follow.php
@@ -7,7 +7,7 @@ session_start();
 if ($_SESSION['z1'] == true) {
   $error = 0;
   $followid = $_POST['followid'];
-	require "danesql.php";
+  require "danesql.php";
   $connect = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME);
   $login = $_SESSION['user'];
   $result = $connect -> query("SELECT * FROM `viddle_users` WHERE login='$login';");
@@ -17,18 +17,19 @@ if ($_SESSION['z1'] == true) {
   if ($userid == $followid) {
     echo "Wystąpił błąd - nastąpiła próba zaobserwowania samego siebie.";
   }
-	if ($result = @$connect->query(sprintf("SELECT * FROM viddle_users WHERE uid='%s", mysqli_real_escape_string($connect,$followid)))) $d2 = $result->num_rows;
-	if (isset($d2) && $d2 == '0') {
-		echo "Wystąpił błąd - nieprawidłowe ID użytkownika.";
+  if ($result = @$connect->query(sprintf("SELECT * FROM viddle_users WHERE uid='%s", mysqli_real_escape_string($connect,$followid))))
+  $d2 = $result->num_rows;
+  if (isset($d2) && $d2 == '0') {
+    echo "Wystąpił błąd - nieprawidłowe ID użytkownika.";
     break 1;
-	}
-	if ($isfollowing = @$connect->query(sprintf("SELECT * FROM viddle_followers WHERE followed='$followid' AND follower='$userid';")) {
+  }
+  if ($isfollowing = @$connect->query(sprintf("SELECT * FROM viddle_followers WHERE followed='$followid' AND follower='$userid';")) {
     $d2 = $isfollowing->num_rows;
     if (isset($d2) && $d2 == '0') {
-		  $success = $connect->query("DELETE FROM viddle_followers WHERE followed='$followid' AND follower='$userid';");
+      $success = $connect->query("DELETE FROM viddle_followers WHERE followed='$followid' AND follower='$userid';");
       echo "Użytkownik odobserwowany pomyślnie.";
       break 2;
-	  } else {
+    } else {
       $success = $connect->query("INSERT INTO viddle_followers VALUES (0, followed='$followid' AND follower='$userid');");
       echo "Użytkownik zaobserwowany pomyślnie.";
       break 2;
