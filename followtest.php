488cfda242dd81af842039e326b9c4ddc2ddead1
Lort533 <76491869+Lort533@users.noreply.github.com>
2021-02-08 19:44:41 +0100

    Update followtest.php

--- followtest.php
+++ followtest.php
@@ -1,6 +1,6 @@
 Wpisz ID osoby którą chcesz obserwować:
 <form action="/follow.php" method="POST">
-<input id="followid" name="followid" type="hidden" value="5fd62da0d95545fd62da0d9557">
-<!--<button type="submit">Obserwuj</button>-->
-<button type="submit" class="btn btn-success"><p style="margin: 10px;">Obserwuj</p></button>
+<input id="followid" name="followid" type="input" value="">
+<button type="submit">Obserwuj</button>
+<!--<button type="submit" class="btn btn-success"><p style="margin: 10px;">Obserwuj</p></button>-->
 </form>
