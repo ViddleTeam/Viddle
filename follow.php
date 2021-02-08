c197fdd4ef8245faca73fef7ea600aa3e87117c0
Lort533 <76491869+Lort533@users.noreply.github.com>
2021-02-08 14:31:07 +0100

    Follow system can be tested now.

---
+++ follow.php
@@ -0,0 +1,42 @@
+<script async src="https://arc.io/widget.min.js#oxtrzHwy"></script>
+<?php
+if ($_POST['followid'] == false) {
+    header('Location: index.php');
+}
+session_start();
+if ($_SESSION['z1'] == true) {
+  $error = 0;
+  $followid = $_POST['followid'];
+	require "danesql.php";
+  $connect = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME);
+  $login = $_SESSION['user'];
+  $result = $connect -> query("SELECT * FROM `viddle_users` WHERE login='$login';");
+  $dane = $result -> fetch_assoc();
+  $userid = $dane['uid'];
+  $login = $_SESSION['user'];
+  if ($userid == $followid) {
+    echo "Wystąpił błąd - nastąpiła próba zaobserwowania samego siebie.";
+  }
+	if ($result = @$connect->query(sprintf("SELECT * FROM viddle_users WHERE uid='%s", mysqli_real_escape_string($connect,$followid)))) $d2 = $result->num_rows;
+	if (isset($d2) && $d2 == '0') {
+		echo "Wystąpił błąd - nieprawidłowe ID użytkownika.";
+    break 1;
+	}
+	if ($isfollowing = @$connect->query(sprintf("SELECT * FROM viddle_followers WHERE followed='$followid' AND follower='$userid';")) {
+    $d2 = $isfollowing->num_rows;
+    if (isset($d2) && $d2 == '0') {
+		  $success = $connect->query("DELETE FROM viddle_followers WHERE followed='$followid' AND follower='$userid';");
+      echo "Użytkownik odobserwowany pomyślnie.";
+      break 2;
+	  } else {
+      $success = $connect->query("INSERT INTO viddle_followers VALUES (0, followed='$followid' AND follower='$userid');");
+      echo "Użytkownik zaobserwowany pomyślnie.";
+      break 2;
+    }
+  }
+} else {
+//header('Location: index.php');
+echo('Wystąpił błąd - użytkownik zalogowany.');
+}
+echo('Jeżeli trafiłeś tutaj przez przypadek, to i tak nic tutaj nie ma ciekawego.');
+?>
