e12e1abaf0fe6dce8d0a468f4b852d5b3203e36b
Lort533 <76491869+Lort533@users.noreply.github.com>
2021-02-08 14:51:48 +0100

    Update follow.php

--- follow.php
+++ follow.php
@@ -17,12 +17,12 @@ if ($_SESSION['z1'] == true) {
   if ($userid == $followid) {
     echo('Wystąpił błąd - nastąpiła próba zaobserwowania samego siebie.');
   }
-  if ($result = @$connect->query(sprintf("SELECT * FROM viddle_users WHERE uid='%s", mysqli_real_escape_string($connect,$followid))))
+  /*if ($result = @$connect->query(sprintf("SELECT * FROM viddle_users WHERE uid='%s", mysqli_real_escape_string($connect,$followid))))
   $d2 = $result->num_rows;
   if (isset($d2) && $d2 == '0') {
     echo('Wystąpił błąd - nieprawidłowe ID użytkownika.');
     break 1;
-  }
+  }*/
   if ($isfollowing = @$connect->query(sprintf("SELECT * FROM viddle_followers WHERE followed='$followid' AND follower='$userid'")) {
     $d2 = $isfollowing->num_rows;
     if (isset($d2) && $d2 == '0') {
