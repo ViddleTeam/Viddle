f91adbc08fc3e1af5c2cab14036cb6a8c8cc8ee8
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-02-09 09:03:07 +0100

    bugfix

--- trending.php
+++ trending.php
@@ -104,11 +104,7 @@ if ($result = @$connect->query("SELECT * FROM `viddle_videos` WHERE `video_id` N
                     </a>
                 </div>
             </div>
-            <footer class="page-footer font-small" style="background-color: #212121; margin-top: 30px;">
-                <div class="footer-copyright text-center text-white py-3">
-                    &copy; 2020-2021 Viddle Developers<br>
-                    <a href="/attachments/privacypolicy.pdf">Polityka prywatności</a> • <a href="/attachments/termsofservice.pdf">Regulamin</a> • <a href="https://discord.gg/QsrbDtxWpn">Serwer Discord</a> • <a href="https://tipply.pl/u/SlaVistaPL">Wesprzyj rozwój Viddle</a><br>
-                    Kontakt mailowy: <b>support@viddle.xyz</b>
-                </div>
-            </footer>
+<?php
+require_once('partials/footer.php');
+?>
 <div class="hiddendiv common"></div></body></html>
