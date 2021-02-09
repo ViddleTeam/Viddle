1a178e360d78e415a31d70a4cce992211a1a9478
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-02-09 08:57:49 +0100

    trending page fix

--- trending.php
+++ trending.php
@@ -110,6 +110,7 @@ if ($result = @$connect->query("SELECT * FROM `viddle_videos` WHERE `video_id` N
                     </div>
                     </a>
                 </div>
+           </div>
                
 		<?php 
 require_once('partials/footer.php');
