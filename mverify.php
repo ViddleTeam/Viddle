92d9463c82b1a4d9261faf5bd368fba930c2d7ec
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-02-09 10:16:08 +0100

    Update mverify.php

--- mverify.php
+++ mverify.php
@@ -9,14 +9,26 @@ if(!isset($_SESSION['uid'])) {
 header('location: index.php');
 } else {
   require 'danesql.php';
-  $connect = new mysqli(SQLOST, SQLUSER, SQLPASS, DBNAME);
+  $connect = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME);
   
   if ($connect->connect_errno!=0)
 	{
 				echo '<span style="color: red;">Wystąpił nieoczekiwany błąd! <br> Skontaktuj się z supportem i podaj ten kod:'.$connect->connect_errno.'</span>';
         exit();
 	} else {
-  }
+	  $uid = $_SESSION['uid'];
+	 $result = $connect->query("SELECT * FROM viddle_users WHERE uid='$uid' AND emailver='0'");
+	  
+	  $d2 = $result->num_rows;
+	  
+	  if($d2 == '1') {
+		 $dane = $result->fetch_assoc();
+		  
+		  
+	  } else {
+		  header('location: index.php');
+	  }
+  	}
   
   
 }
