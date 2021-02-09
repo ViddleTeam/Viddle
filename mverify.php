b1b3e729003a204f94b0055ff32a0642bb20f732
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-02-09 09:15:12 +0100

    Update mverify.php

--- mverify.php
+++ mverify.php
@@ -5,6 +5,22 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+if(!isset($_SESSION['uid'])) {
+header('location: index.php');
+} else {
+  require 'danesql.php';
+  $connect = new mysqli(SQLOST, SQLUSER, SQLPASS, DBNAME);
+  
+  if ($connect->connect_errno!=0)
+	{
+				echo '<span style="color: red;">Wystąpił nieoczekiwany błąd! <br> Skontaktuj się z supportem i podaj ten kod:'.$connect->connect_errno.'</span>';
+        exit();
+	} else {
+  }
+  
+  
+}
+
 
 ?>
 
