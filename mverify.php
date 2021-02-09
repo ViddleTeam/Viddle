2d49c942aa27030a601c92d6c1594c979c81a602
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-02-09 13:35:03 +0100

    Update mverify.php

--- mverify.php
+++ mverify.php
@@ -1,10 +1,6 @@
 <?php
 session_start();
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
 // Load Composer's autoloader
 include 'vendor/autoload.php';
 
