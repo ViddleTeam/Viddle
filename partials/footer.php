08933ba617b0ac2cbc418f38f209dde8c0308c92
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-02-09 09:02:40 +0100

    footer fix

--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,5 @@
 <!-- <link rel="stylesheet" href="../assets/master.css" /> -->
-<footer class="page-footer font-small" style="background-color: #212121; margin-top: 30px; min-width: 100% !important;">
+<footer class="page-footer font-small" style="background-color: #212121; margin-top: 30px;">
   <div class="footer-copyright text-center text-white py-3">
       &copy; 2020-2021 Viddle Developers<br>
       <a href="/attachments/privacypolicy.pdf">Polityka prywatności</a> • <a href="/attachments/termsofservice.pdf">Regulamin</a> • <a href="https://discord.gg/QsrbDtxWpn">Serwer Discord</a> • <a href="https://tipply.pl/u/SlaVistaPL">Wesprzyj rozwój Viddle</a><br>
