8ad42559b9943ecda4ee1306970d2a6e023b61e9
MinecraftCiach <ciach795@gmail.com>
2021-02-17 00:58:04 +0100

    small update including inputs

--- videonew.php
+++ videonew.php
@@ -5,7 +5,10 @@ $db = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME);
 $id = $_GET['id'];
 $_SESSION['id'] = $_GET['id'];
 $already_seen = false;
-$user_ip = $_SERVER['HTTP_CLIENT_IP'] OR $_SERVER['HTTP_X_FORWARDED_FOR'] OR $_SERVER['REMOTE_ADDR'];
+$user_ip = null;
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) $user_ip = $_SERVER['HTTP_CLIENT_IP'];
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) $user_ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+else $user_ip = $_SERVER['REMOTE_ADDR'];
 $polecenie = sprintf("SELECT * FROM viddle_videos WHERE video_id = %s", $db->real_escape_string($id));
 if ($result = $db->query($polecenie)) {
     $rows = $result->num_rows;
