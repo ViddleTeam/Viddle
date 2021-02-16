0219cde40089acf3afd7627f0563e08fa3948494
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-02-16 09:53:31 +0100

    Update video.php

--- video.php
+++ video.php
@@ -307,7 +307,7 @@ if ($video_e == true)
                         } ?>
                       </span>
                                 <form method="post">
-                      <span class="md-form my-0 mx-2" style="color: white !important;">
+                      <span class="md-form my-0 mx-2" style="color: white !important; margin: -35px 15px 0 0;">
                           <input class="form-control mr-sm-2 d-none d-lg-block" style="color: white !important; width: 32rem;" type="text" placeholder="Dodaj komentarz" aria-label="Dodaj komentarz">
 			              <input class="form-control mr-sm-2 d-lg-none" style="color: white !important; width: 100%;" type="text" placeholder="Dodaj komentarz" aria-label="Dodaj komentarz" name="tresc1">
                       </span>
