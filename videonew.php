6ddc5edd968f7ce7d88e6070b21355a1d86e41ce
MinecraftCiach <ciach795@gmail.com>
2021-02-17 00:44:00 +0100

    small update including inputs

---
+++ videonew.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+require 'danesql.php';
+$db = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME);
+$id = $_GET['id'];
+$_SESSION['id'] = $_GET['id'];
+$already_seen = false;
+$polecenie = sprintf("SELECT * FROM viddle_videos WHERE id = %s", $db->real_escape_string($id));
+if ($result = $db->query($polecenie)) {
+    $rows = $result->num_rows;
+    echo "$result";
+}
