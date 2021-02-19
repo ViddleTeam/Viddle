f239cb9ca95cf0f901f49e56ab66264dd12550ba
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-02-19 17:03:21 +0100

    Update post.php

--- post.php
+++ post.php
@@ -191,7 +191,17 @@ if ($_SESSION['z1'] == true) {
 	  
 	  if (in_array($file_ext, $allowed_file_types) && ($filesize < 1*GB))
 	  {
-	  $success = $connect->query("INSERT INTO viddle_videos VALUES (0, '$wstaw', '$userid', 123454321, '$viddleid', 0, 0, 0, 0, '$newfilename', '$zabezpdwa', '$zabezptrzy', '$t', '$data')");
+	  //$success = $connect->query("INSERT INTO viddle_videos VALUES (0, '$wstaw', '$userid', 123454321, '$viddleid', 0, 0, 0, 0, '$newfilename', '$zabezpdwa', '$zabezptrzy', '$t', '$data')");
+	  	if ($success = @$connect->query(
+		    sprintf("INSERT INTO viddle_videos VALUES (0, '%s', '%s', 123454321, '%s', 0, 0, 0, 0, '%s', '%s', '%s', '%s', '%s'",
+		    mysqli_real_escape_string($connect,$wstaw),
+		    mysqli_real_escape_string($connect,$userid),
+		    mysqli_real_escape_string($connect,$viddleid),
+	            mysqli_real_escape_string($connect,$newfilename),
+		    mysqli_real_escape_string($connect,$zabezpdwa),
+	            mysqli_real_escape_string($connect,$zabezptrzy),
+	            mysqli_real_escape_string($connect,$t),
+	            mysqli_real_escape_string($connect,$data))))
 	  }
 	  if ($success) {
 	     $successtwo = $connect->query("UPDATE viddle_recent SET viddle_recent_three_user=viddle_recent_two_user,viddle_recent_three_id=viddle_recent_two_id,viddle_recent_two_user=viddle_recent_one_user,viddle_recent_two_id=viddle_recent_one_id,viddle_recent_one_user='$userid',viddle_recent_one_id='$viddleid' WHERE number = 1;");
