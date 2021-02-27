a92f8016e4fa490a3f86198bef41bd5abb604349
MinecraftCiach <ciach795@gmail.com>
2021-02-27 20:04:43 +0100

    Update

--- videonew.php
+++ videonew.php
@@ -5,8 +5,10 @@ require_once 'partials/navbar.php';
 $db = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME);
 $id = $_GET['id'];
 $_SESSION['id'] = $_GET['id'];
+$res = null;
 $stmt = sprintf("SELECT * FROM viddle_videos WHERE video_id = '%s'", $db->real_escape_string($id));
 if ($result = $db->query($stmt)) {
-    echo $result;
+    $res = $result;
 }
 ?>
+<p><?= $res ?></p>
