e2996865eff2fc085f66e3e6a37843dca88a6906
MinecraftCiach <ciach795@gmail.com>
2021-02-27 20:03:40 +0100

    Update

--- videonew.php
+++ videonew.php
@@ -5,17 +5,8 @@ require_once 'partials/navbar.php';
 $db = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME);
 $id = $_GET['id'];
 $_SESSION['id'] = $_GET['id'];
-$already_seen = false;
-$user_ip = null;
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) $user_ip = $_SERVER['HTTP_CLIENT_IP'];
-elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) $user_ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-else $user_ip = $_SERVER['REMOTE_ADDR'];
-$polecenie = sprintf("SELECT * FROM viddle_videos WHERE video_id = %s", $db->real_escape_string($id));
-if ($result = $db->query($polecenie)) {
-    $rows = $result->num_rows;
+$stmt = sprintf("SELECT * FROM viddle_videos WHERE video_id = '%s'", $db->real_escape_string($id));
+if ($result = $db->query($stmt)) {
+    echo $result;
 }
 ?>
-<p>Witaj na nowej stronie Video!</p>
-<form method="post" action="videonew">
-
-</form>
