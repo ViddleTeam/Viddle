9617de217f8d95106db08d651901adbe5460eb5b
MinecraftCiach <ciach795@gmail.com>
2021-02-27 21:09:10 +0100

    Update

--- videonew.php
+++ videonew.php
@@ -5,25 +5,26 @@ require_once 'partials/navbar.php';
 $db = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME);
 $id = $_GET['id'];
 $_SESSION['id'] = $_GET['id'];
-
 $stmt = sprintf("SELECT * FROM viddle_videos WHERE video_id = '%s'", $db->real_escape_string($id));
-if ($result = $db->query($stmt)) {
-    [
-        'title' => $title,
-        'opis' => $description,
-        'publisher' => $publisher,
-        'video_id' => $video_id,
-        'publishdate' => $published_at,
-        'upvotes' => $upvotes,
-        'downvotes' => $downvotes
-    ] = $result->fetch_assoc();
-}
+[
+    'title' => $title,
+    'opis' => $description,
+    'publisher' => $publisher,
+    'video_id' => $video_id,
+    'publishdate' => $published_at,
+    'upvotes' => $upvotes,
+    'downvotes' => $downvotes
+] = $db->query($stmt)->fetch_assoc();
+$user_stmt = sprintf("SELECT login FROM viddle_users WHERE uid = '%s'", $db->real_escape_string($publisher));
+[
+        'login' => $username,
+] = $db->query($stmt)->fetch_assoc();
 ?>
 <div style="margin-top: 60px;" class="container">
     <p>
         Title => <?= $title ?>
         Description => <?= $description ?>
-        Publisher => <?= $publisher ?>
+        Publisher => <?= $publisher ?> | <?= $username ?>
         Video ID => <?= $video_id ?>
         Published at => <?= $published_at ?>
         Upvotes => <?= $upvotes ?>
