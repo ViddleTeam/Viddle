7569dfdaac6ea058d93cc28677cc9bdf6d0f9a75
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-02-28 15:22:00 +0100

    Update video.php

--- video.php
+++ video.php
@@ -305,8 +305,139 @@ if ($video_e == true) {
                                 <span style="margin-left: auto; margin-right: -20px; text-align: right;">
                     	<h4 style="font-weight: bold;"><i class="fas fa-eye" style="margin-right: 5px;"></i> <?php echo $views ?></h4>
 
-                      <span style='margin-right: 5px;'><a style='<?php echo $disable ?>' href="like.php?id=<?php echo $id ?>"><i class="fas fa-arrow-up" <?php echo $like ?>"></i> <?php echo $likes ?></a></span> <a style='<?php echo $disable ?>' href="dislike.php?id=<?php echo $id ?>"><i class="fas fa-arrow-down" style="<?php echo $dislike ?> margin-left: 10px; margin-right: 5px;"></i> <?php echo $dislikes ?></a>
+                      <span style='margin-right: 5px;'><div onclick="like()" style='<?php echo $disable ?>' href="like.php?id=<?php echo $id ?>"><i class="fas fa-arrow-up" <?php echo $like ?>"></i> <?php echo $likes ?></div></span> <a style='<?php echo $disable ?>' href="dislike.php?id=<?php echo $id ?>"><i class="fas fa-arrow-down" style="<?php echo $dislike ?> margin-left: 10px; margin-right: 5px;"></i> <?php echo $dislikes ?></a>
                     </span>
+			      <script>
+			      function like() {
+			      <?php
+	    			session_start();
+
+if(!isset($_SESSION['uid'])) {
+	$_SESSION['pol'] = true;
+	header('location: video.php?id='.$id.'');
+} else {
+
+require 'danesql.php';
+$connect = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME);
+$uid = $_SESSION['uid'];
+
+$r = '1';
+
+if ($result = @$connect->query(
+sprintf("SELECT * FROM viddle_videos WHERE video_id='%s'",
+mysqli_real_escape_string($connect,$id)))) {
+$d2 = $result->num_rows;
+$dane = $result->fetch_assoc();
+	
+$like = $dane['upvotes'];
+$dislike = $dane['downvotes'];
+	
+if($d2 == '1')
+{
+	if ($result1 = @$connect->query(
+	sprintf("SELECT * FROM viddle_oceny WHERE videoid='%s' AND uid='%s' AND ocena='%s'",
+	mysqli_real_escape_string($connect,$id),
+	mysqli_real_escape_string($connect,$_SESSION['uid']),
+	mysqli_real_escape_string($connect,$r)))) 
+	{
+		$d3 = $result1->num_rows;
+		
+		if($d3 == '1')
+		{
+			echo '1';
+			$w = $like - '1';
+			if($gg = @$connect->query(
+			sprintf("DELETE FROM viddle_oceny WHERE videoid='%s' AND uid='%s'",
+			mysqli_real_escape_string($connect,$id),
+			mysqli_real_escape_string($connect,$uid)))) {
+				echo '2';
+				
+				if($gg = @$connect->query(
+				sprintf("UPDATE viddle_videos SET upvotes='%s' WHERE video_id='%s'",
+				mysqli_real_escape_string($connect,$w),
+				mysqli_real_escape_string($connect,$id)))) {
+					echo '3';
+					header('location: video.php?id='.$id.'');
+				}
+					
+			}
+		} else {
+			$o = '0';
+			if ($result2 = @$connect->query(
+			sprintf("SELECT * FROM viddle_oceny WHERE videoid='%s' AND uid='%s' AND ocena='%s'",
+			mysqli_real_escape_string($connect,$id),
+			mysqli_real_escape_string($connect,$_SESSION['uid']),
+			mysqli_real_escape_string($connect,$o)))) 
+			{
+				$w = 
+				$d4 = $result2->num_rows;
+				
+				if($d4 == '1')
+				{
+						echo '6';
+			$w = $dislike - '1';
+			$w2 = $like + '1';
+			if($gg = @$connect->query(
+			sprintf("UPDATE viddle_oceny SET ocena='%s' WHERE videoid='%s' AND uid='%s'",
+			mysqli_real_escape_string($connect,$r),
+			mysqli_real_escape_string($connect,$id),
+			mysqli_real_escape_string($connect,$uid)))) {
+				echo '2';
+				if($gg = @$connect->query(
+			sprintf("UPDATE viddle_videos SET downvotes='%s' WHERE video_id='%s'",
+			mysqli_real_escape_string($connect,$w),
+			mysqli_real_escape_string($connect,$id)))) {
+					
+					if($gg = @$connect->query(
+			sprintf("UPDATE viddle_videos SET upvotes='%s' WHERE video_id='%s'",
+			mysqli_real_escape_string($connect,$w2),
+			mysqli_real_escape_string($connect,$id)))) {
+						
+						echo '3';
+				
+					header('location: video.php?id='.$id.'');
+						
+					}
+					
+				}
+				
+					
+			}
+					
+				} else {
+					$d = '1';
+					if ($syf = @$connect->query(
+					sprintf("INSERT INTO `viddle_oceny` VALUES (NULL,'%s','%s','%s')",
+					mysqli_real_escape_string($connect,$uid),
+					mysqli_real_escape_string($connect,$id),
+					mysqli_real_escape_string($connect,$d))))
+					{
+						$w = $like + '1';
+						if($gg = @$connect->query(
+						sprintf("UPDATE viddle_videos SET upvotes='%s' WHERE video_id='%s'",
+						mysqli_real_escape_string($connect,$w),
+						mysqli_real_escape_string($connect,$id)))) {
+							echo '3';
+							header('location: video.php?id='.$id.'');
+						}
+						
+					}
+				}
+			}
+			
+			
+		}
+	}
+		
+} else {
+	header('location: index.php');
+}
+
+}
+}	
+	    			?>
+			      }
+			      </script>
                                 <?php echo $powod ?>
                             </div><br>
                             <p><?php echo $opis ?></p>
