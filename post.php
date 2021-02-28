07afe2972829c3c6e50c82f33806ac12e9ccbba5
Lort533 <76491869+Lort533@users.noreply.github.com>
2021-02-28 23:57:29 +0100

    Update post.php

--- post.php
+++ post.php
@@ -147,7 +147,7 @@ if ($_SESSION['z1'] == true) {
 	      ftp_close($conn_id);
 	      $videocompress = $_FILES["videovid"]["tmp_name"];
 	      $compresscommand = "/usr/bin/ffmpeg -i $videocompress -b:v 5000k -bufsize 5000k ftp://" . FTPUSER . ":" . FTPPASS . "@" . FTPSERWER . ":21/videos/test/output.mp4 2>&1";
-	      system($compresscommand, $returnedvalue);
+	      $returnedvalue = shell_exec($compresscommand);
 	      //echo "Wrzucono film.";
 	      $uplsuccess = 1;
 	    }
@@ -191,7 +191,7 @@ if ($_SESSION['z1'] == true) {
 	  
 	  if (in_array($file_ext, $allowed_file_types) && ($filesize < 1*GB))
 	  {
-	  $success = $connect->query("INSERT INTO viddle_videos VALUES (0, '$wstaw', '$userid', 123454321, '$viddleid', 0, 0, 0, 0, '$newfilename', '$returnedvalue', '$zabezptrzy', '$rozszerzenieminiatura', '$data')");
+	  $success = $connect->query("INSERT INTO viddle_videos VALUES (0, '$wstaw', '$userid', 123454321, '$viddleid', 0, 0, 0, 0, '$newfilename', '$zabezpdwa', '$returnedvalue', '$rozszerzenieminiatura', '$data')");
 	  }
 	  if ($success) {
 	     $successtwo = $connect->query("UPDATE viddle_recent SET viddle_recent_three_user=viddle_recent_two_user,viddle_recent_three_id=viddle_recent_two_id,viddle_recent_two_user=viddle_recent_one_user,viddle_recent_two_id=viddle_recent_one_id,viddle_recent_one_user='$userid',viddle_recent_one_id='$viddleid' WHERE number = 1;");
