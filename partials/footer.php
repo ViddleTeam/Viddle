fca00dedc68198676d57d0cb43cf396a8cfd95a9
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-03-02 19:08:35 +0100

    poprawki

--- partials/footer.php
+++ partials/footer.php
@@ -9,8 +9,8 @@
 
 <div class="modal fade" id="supportModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
   aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content bg-dark">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Wesprzyj rozwój Viddle</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -26,7 +26,7 @@
         </ul>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Zamknij</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal" style="padding: 10px;">Zamknij</button>
       </div>
     </div>
   </div>
