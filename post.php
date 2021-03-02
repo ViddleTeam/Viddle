061284ece1422d4c1631b4a00b0e4fcf35014316
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-03-02 13:12:19 +0100

    Update post.php

--- post.php
+++ post.php
@@ -137,10 +137,10 @@ if ($_SESSION['z1'] == true) {
 	      echo "Plik już istnieje.";
 	      //header('Location: blad.php?id=4');
 	    } else {
-	    if($rozszerzenieminiatura == 'x') {
+	   
 	      ftp_chdir($conn_id, '/videos/');
 	      ftp_mkdir($conn_id, $viddleid);
-	    }
+	    
 	      ftp_chdir($conn_id, '/videos/'.$viddleid.'/');
 	      ftp_put($conn_id, $newfilename, $_FILES["videovid"]["tmp_name"], FTP_BINARY); 
 	      //echo "Wrzucono film.";
