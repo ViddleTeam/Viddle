7f8f74f13717d82ede05770468db5ddd1e3c1587
Lort533 <76491869+Lort533@users.noreply.github.com>
2021-03-05 08:00:44 +0100

    Update deletevidprivatepanel.php

--- deletevidprivatepanel.php
+++ deletevidprivatepanel.php
@@ -1,7 +1,20 @@
 <html>
 <head>
+    <script async src="https://arc.io/widget.min.js#oxtrzHwy"></script>
     <link rel="stylesheet" href="style.css">
     <title>Usuń film z Viddle</title>
+    <style>
+    body {
+      top: 50%;
+      left: 50%;
+    }
+    button {
+        outline: none;
+        margin: 10px;
+        height: 50px;
+        width: 225px;
+    }
+    </style>
 </head>
 <body>
     <form action="deletevidsecret.php" method="post">
