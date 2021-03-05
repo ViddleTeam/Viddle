861c5560c5499983dc7d3ae54fc3459460370e18
Lort533 <76491869+Lort533@users.noreply.github.com>
2021-03-05 07:51:03 +0100

    Update danesql.php

--- danesql.php
+++ danesql.php
@@ -3,3 +3,4 @@ define('SQLHOST', 'localhost');
 define('SQLPASS', 'zLjOJcX2sjpqaXqq');
 define('SQLUSER', 'viddle');
 define('DBNAME', 'viddle');
+define('PRIVATE_TOKEN', 'CH4t7e0?tr!b6S_va_Huf&itU4wustUmabRl$E$+aS2L3+?Jasaph&0e6');
