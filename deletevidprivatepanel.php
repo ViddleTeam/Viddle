fa13f0de74916d2baf6a0ec7d35df1e707789a4c
Lort533 <76491869+Lort533@users.noreply.github.com>
2021-03-05 07:57:26 +0100

    Create deletevidprivatepanel.php

---
+++ deletevidprivatepanel.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Usuń film z Viddle</title>
+</head>
+<body>
+    <form action="deletevidsecret.php" method="post">
+    <h2>Kod filmu: <input type="text" id="kodf" name="kodf"></h2>
+    <br /><h2>Token zabezpieczający: <input type="text" id="token" name="token"></h2>
+    <input type="submit" value="Usuń film">
+</body>
+</html>
