0e4052bfd125009e4b5c930f57cb62bbcb4475b9
Lort533 <76491869+Lort533@users.noreply.github.com>
2021-03-05 08:06:24 +0100

    Update deletevidprivatepanel.php

--- deletevidprivatepanel.php
+++ deletevidprivatepanel.php
@@ -22,7 +22,7 @@
     <center>
     <form action="deletevidsecret.php" method="post">
     <h2>Kod filmu: <input type="text" id="kodf" name="kodf" /></h2>
-    <br /><h2>Token zabezpieczający: <input type="text" id="token" name="token" /></h2>
+    <h2>Token zabezpieczający: <input type="text" id="token" name="token" /></h2>
     <input type="submit" value="Usuń film" />
     </form>
     </center>
