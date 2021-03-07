591129b2f923f9ebd26a1ffa789947ea80183321
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-03-07 13:31:24 +0100

    Update deletevid.php

--- deletevid.php
+++ deletevid.php
@@ -1,46 +1,36 @@
 <?php 
 session_start();
 $id = $_GET['id'];
-include 'danesql.php';
-include 'daneftp.php';
+require 'danesql.php';
 
-if(!isset($_SESSION['uid'])) {
-  header('location: index.php');
-  exit();
-}
-$uid = $_SESSION['uid'];
-$connect = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME);
-if ($result = @$connect->query(
-sprintf("SELECT * FROM `viddle_videos` WHERE `publisher`='%s' AND `video_id`='%s'",
-mysqli_real_escape_string($connect,$uid),
-mysqli_real_escape_string($connect,$id)))) {
-
-  $dane = $res->fetch_assoc();
-	$d2 = $res->num_rows;
-echo $il;
-echo $dane;
-  if($il == '1') {
-    if($connect->query("DELETE * FROM viddle_videos WHERE video_id='$id'")) {
-      $ftp_server = FTPSERWER;
-			$ftp_conn = ftp_connect($ftp_server) or die("Wystąpił błąd! Skontaktuj się z supportem.");
-      $login = ftp_login($ftp_conn, FTPUSER, FTPPASS);
-      ftp_chdir($ftp_conn, "videos");
-      ftp_chdir($ftp_conn, $dane['video_id']);
-      ftp_delete($ftp_conn, $dane['fname']);
-      ftp_chdir($ftp_conn, "/videos");
-      ftp_rmdir($ftp_conn, $dane['video_id']);
-      ftp_close($ftp_conn);
-      header('location: settings.php');
-  } else {
-	    header('location: index1.php');
-    }
-  } else {
-	  echo 'mysql query error';
-  }
-
-  
+if($connect = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME)) {
+	if(isset($_SESSION['uid'])) {
+		$uid = $_SESSION['uid'];
+		if($result = @$connect->query("SELECT * FROM `viddle_videos` WHERE `publisher`='$uid' AND `video_id`='$id'")) {
+			$d2 = $result->num_rows;
+			
+			if($d2 == '1') {
+				if($connect->query("DELETE * FROM `viddle_videos` WHERE `video_id`='$id'")) {
+					require 'daneftp.php';
+					$ftp_server = FTPSERWER;
+					$ftp_conn = ftp_connect($ftp_server) or die("Wystąpił błąd! Skontaktuj się z supportem.");
+					$login = ftp_login($ftp_conn, FTPUSER, FTPPASS);
+					ftp_chdir($ftp_conn, "videos");
+					ftp_chdir($ftp_conn, $dane['video_id']);
+					ftp_delete($ftp_conn, $dane['fname']);
+					ftp_chdir($ftp_conn, "/videos");
+					ftp_rmdir($ftp_conn, $dane['video_id']);
+					ftp_close($ftp_conn);
+					header('location: settings.php');
+				}
+			} else {
+				header('location: index.php');
+			}    
+		}
+	} else {
+		header('location: index.php');
+	}
 } else {
-  echo 'error';
+	echo 'mysqli connect error';
 }
-
 ?>
