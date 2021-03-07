21bd6a9dd0fe636b8a3f2b3adaba159455c4543a
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-03-07 13:23:42 +0100

    Update deletevid.php

--- deletevid.php
+++ deletevid.php
@@ -1,4 +1,5 @@
-<?php session_start();
+<?php 
+session_start();
 $id = $_GET['id'];
 include 'danesql.php';
 include 'daneftp.php';
@@ -36,8 +37,10 @@ echo $dane;
   } else {
 	  echo 'mysql query error';
   }
+
   
 } else {
   echo 'error';
 }
-  ?>
+
+?>
