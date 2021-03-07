efbb3cc07e64970441788cea78702c51e6f48750
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-03-07 12:54:45 +0100

    Update deletevid.php

--- deletevid.php
+++ deletevid.php
@@ -1,8 +1,33 @@
-<?php 
-session_start();
-require 'daneftp.php';
-require 'danesql.php';
-try {
-} catch (exception $e) {
+<?php session_start();
+$id = $_GET['id'];
+include 'danesql.php';
+include 'daneftp.php';
+
+if(!isset($_SESSION['uid'])) {
+  header('location: index.php');
+  exit();
 }
-?>
+$connect = new mysqli(SQLHOST, SQLUSER, SQLPASS, DBNAME);
+if($res = $connect->query("SELECT * FROM viddle_videos WHERE video_id='$id'")) {
+  $dane = $res->fetch_assoc();
+
+  if($dane['publisher'] == $_SESSION['uid']) {
+    if($connect->query("DELETE * FROM viddle_videos WHERE video_id='$id'")) {
+      $ftp_server = FTPSERWER;
+			$ftp_conn = ftp_connect($ftp_server) or die("Wystąpił błąd! Skontaktuj się z supportem.");
+      $login = ftp_login($ftp_conn, FTPUSER, FTPPASS);
+      ftp_chdir($ftp_conn, "videos");
+      ftp_chdir($ftp_conn, $dane['video_id']);
+      ftp_delete($ftp_conn, $dane['fname']);
+      ftp_chdir($ftp_conn, "/videos");
+      ftp_rmdir($ftp_conn, $dane['video_id']);
+      ftp_close($ftp_conn);
+      header('location: settings.php');
+  } 
+  } else {
+    header('location: index.php');
+  }
+} else {
+  echo 'error';
+}
+  ?>
