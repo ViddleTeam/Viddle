f478edb04ea5b9a7b0d98c54ed8d373598a63cfd
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-03-07 12:59:52 +0100

    kolejne poprawki

--- partials/navbar.php
+++ partials/navbar.php
@@ -153,7 +153,7 @@ if($block == '1')
                         <?php
                     } else { ?>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img width="32px" style="border-radius:50%;margin-right:5px;" class="img-responsive" src="<?php echo $av3 ?>"></a>
+                        <a class="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img width="32px" height="32px" style="border-radius:50%;margin-right:5px;" class="img-responsive" src="<?php echo $av3 ?>"></a>
                         <div class="dropdown-menu dropdown-menu-right dropdown-default" style="min-width: 150px;" aria-labelledby="navbarDropdownMenuLink">
                             <a class="dropdown-item waves-effect waves-light" href="channel?id=<?php if (isset($uid)) echo $uid ?>">Strona twojego kanału</a>
                             <a class="dropdown-item waves-effect waves-light" href="creatorstudio">Studio twórców</a>
