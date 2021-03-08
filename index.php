5380eae4a67edf55f3270a225d5313d90e8862f0
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-03-08 09:59:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -245,7 +245,7 @@ $new = $connect->query("SELECT * FROM `viddle_videos` ORDER BY `publishdate` DES
 		    <?php
 		    if(!$new) {
 			   $w = '0';
-			   for($dane = $new->fetch_assoc, $w == '3';){
+			   for($dane = $new->fetch_assoc; $w == '3';){
 				   $w = $w + '1';
 				   $vid = $dane['video_id'];
 				   $uid = $dane['publisher'];
