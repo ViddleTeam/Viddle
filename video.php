0d3aba7dc204da21c9a4fcbff041d29e1ebf7fe2
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-03-14 12:31:33 +0100

    usunięcie pustych linijek w kodzie

--- video.php
+++ video.php
@@ -349,16 +349,10 @@ $dislike = $data['downvotes'];
 
 if($d2 == '1')
 {
-	if ($result1 = @$connect->query(
-	sprintf("SELECT * FROM viddle_oceny WHERE videoid='%s' AND uid='%s' AND ocena='%s'",
-	mysqli_real_escape_string($connect,$id),
-	mysqli_real_escape_string($connect,$_SESSION['uid']),
-	mysqli_real_escape_string($connect,$r)))) 
-	{
+	if ($result1 = @$connect->query(sprintf("SELECT * FROM viddle_oceny WHERE videoid='%s' AND uid='%s' AND ocena='%s'", mysqli_real_escape_string($connect,$id), mysqli_real_escape_string($connect,$_SESSION['uid']), mysqli_real_escape_string($connect,$r)))) {
 		$d3 = $result1->num_rows;
 		
-		if($d3 == '1')
-		{
+		if($d3 == '1') {
 			echo '1';
 			$w = $like - '1';
 			if($gg = @$connect->query(
@@ -378,60 +372,28 @@ if($d2 == '1')
 			}
 		} else {
 			$o = '0';
-			if ($result2 = @$connect->query(
-			sprintf("SELECT * FROM viddle_oceny WHERE videoid='%s' AND uid='%s' AND ocena='%s'",
-			mysqli_real_escape_string($connect,$id),
-			mysqli_real_escape_string($connect,$_SESSION['uid']),
-			mysqli_real_escape_string($connect,$o)))) 
-			{
+			if ($result2 = @$connect->query(sprintf("SELECT * FROM viddle_oceny WHERE videoid='%s' AND uid='%s' AND ocena='%s'", mysqli_real_escape_string($connect,$id), mysqli_real_escape_string($connect,$_SESSION['uid']), mysqli_real_escape_string($connect,$o)))) {
 				$w = 
 				$d4 = $result2->num_rows;
-				
-				if($d4 == '1')
-				{
+				if($d4 == '1') {
 						echo '6';
 			$w = $dislike - '1';
 			$w2 = $like + '1';
-			if($gg = @$connect->query(
-			sprintf("UPDATE viddle_oceny SET ocena='%s' WHERE videoid='%s' AND uid='%s'",
-			mysqli_real_escape_string($connect,$r),
-			mysqli_real_escape_string($connect,$id),
-			mysqli_real_escape_string($connect,$uid)))) {
+			if($gg = @$connect->query(sprintf("UPDATE viddle_oceny SET ocena='%s' WHERE videoid='%s' AND uid='%s'", mysqli_real_escape_string($connect,$r), mysqli_real_escape_string($connect,$id), mysqli_real_escape_string($connect,$uid)))) {
 				echo '2';
-				if($gg = @$connect->query(
-			sprintf("UPDATE viddle_videos SET downvotes='%s' WHERE video_id='%s'",
-			mysqli_real_escape_string($connect,$w),
-			mysqli_real_escape_string($connect,$id)))) {
+				if($gg = @$connect->query(sprintf("UPDATE viddle_videos SET downvotes='%s' WHERE video_id='%s'", mysqli_real_escape_string($connect,$w), mysqli_real_escape_string($connect,$id)))) {
 					
-					if($gg = @$connect->query(
-			sprintf("UPDATE viddle_videos SET upvotes='%s' WHERE video_id='%s'",
-			mysqli_real_escape_string($connect,$w2),
-			mysqli_real_escape_string($connect,$id)))) {
-						
+					if($gg = @$connect->query(sprintf("UPDATE viddle_videos SET upvotes='%s' WHERE video_id='%s'", mysqli_real_escape_string($connect,$w2), mysqli_real_escape_string($connect,$id)))) {
 						echo '3';
-				
-					header('location: video.php?id='.$id.'');
-						
+					    header('location: video.php?id='.$id.'');
 					}
-					
 				}
-				
-					
 			}
-					
 				} else {
 					$d = '1';
-					if ($syf = @$connect->query(
-					sprintf("INSERT INTO `viddle_oceny` VALUES (NULL,'%s','%s','%s')",
-					mysqli_real_escape_string($connect,$uid),
-					mysqli_real_escape_string($connect,$id),
-					mysqli_real_escape_string($connect,$d))))
-					{
+					if ($syf = @$connect->query(sprintf("INSERT INTO `viddle_oceny` VALUES (NULL,'%s','%s','%s')", mysqli_real_escape_string($connect,$uid), mysqli_real_escape_string($connect,$id), mysqli_real_escape_string($connect,$d)))) {
 						$w = $like + '1';
-						if($gg = @$connect->query(
-						sprintf("UPDATE viddle_videos SET upvotes='%s' WHERE video_id='%s'",
-						mysqli_real_escape_string($connect,$w),
-						mysqli_real_escape_string($connect,$id)))) {
+						if($gg = @$connect->query(sprintf("UPDATE viddle_videos SET upvotes='%s' WHERE video_id='%s'", mysqli_real_escape_string($connect,$w), mysqli_real_escape_string($connect,$id)))) {
 							echo '3';
 							header('location: video.php?id='.$id.'');
 						}
