8ee328aacc4df3345964fc57e881f99acd8fd67e
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-03-17 18:38:47 +0100

    Update discover.php

--- discover.php
+++ discover.php
@@ -7,16 +7,13 @@ $blad = '0';
 ?>
       <div class="container" style="margin-top:30px;">
         <div class="row">
-          <div class="col-lg-12" style="display: -webkit-box;display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;flex-direction: column;-webkit-box-align: center;align-items: center;-webkit-box-pack: center;justify-content: center;">
-        </div>
+          <div class="col-lg-12" style="display: -webkit-box;display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;flex-direction: column;-webkit-box-align: center;align-items: center;-webkit-box-pack: center;justify-content: center;"></div>
         </div>
           <div class="row">
               <div class="col-lg-12">
                   <h4 class="tile-before" style="color:white; margin-top: 40px;"><br>Odkrywaj twórców na Viddle</h4>
-              </div>
-            </div>
-	      <?php
-		      
+              </div>	      
+        <?php
 			   $i = '0';
 			   for($i += '1'; $i < '4';){
 				   $los = @$connect->query("SELECT * FROM viddle_videos ORDER BY RAND() LIMIT 1");
@@ -28,8 +25,7 @@ $blad = '0';
 				   $v = @$connect->query("SELECT * FROM viddle_vievs WHERE vid='$vid'");
 				   $vievs = $v->num_rows;
 				   $i = $i + '1';
-				   echo ' <div class="tile" style="margin: auto;">
-                <div class="card">
+				   echo '<div class="card">
                     <a href="video.php?id='.$vid.'">
                     <img src="https://i.pinimg.com/originals/07/03/6e/07036e12e9ca047f542437befa8872d3.jpg" class="img-responsive card-img">
                     <p class="card-title">'.$dane['title'].'</p>
@@ -39,16 +35,11 @@ $blad = '0';
                         <span>•</span>
                         <span>'.$vievs.' wyświetleń</span>
                     </div>
-                    </a>
-                </div>';
+                    </a>';
 			   }
-		      
-	      
 	      ?>
-
         </div>
             <?php
             require_once('partials/footer.php');
             ?>
-
 <div class="hiddendiv common"></div></body></html>
