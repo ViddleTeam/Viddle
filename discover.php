86a4baffa217afa42d2e17b0bab97ad00f80eca5
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-03-17 18:40:40 +0100

    poprawki

--- discover.php
+++ discover.php
@@ -8,8 +8,6 @@ $blad = '0';
       <div class="container" style="margin-top:30px;">
         <div class="row">
           <div class="col-lg-12" style="display: -webkit-box;display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;flex-direction: column;-webkit-box-align: center;align-items: center;-webkit-box-pack: center;justify-content: center;"></div>
-        </div>
-          <div class="row">
               <div class="col-lg-12">
                   <h4 class="tile-before" style="color:white; margin-top: 40px;"><br>Odkrywaj twórców na Viddle</h4>
               </div>      
@@ -38,7 +36,6 @@ $blad = '0';
                     </a>';
 			   }
 	      ?>
-        </div>
         </div>
 <?php
   require_once('partials/footer.php');
