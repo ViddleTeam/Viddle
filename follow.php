b6a456f2fd84771da5e088d6d3d4300b37d1d47e
MinecraftCiach <ciach795@gmail.com>
2021-03-18 20:08:56 +0100

    Update

--- follow.php
+++ follow.php
@@ -43,13 +43,17 @@ if ($_SESSION['z1'] == true) {
 }
 //echo('Jeżeli trafiłeś tutaj przez przypadek, to i tak nic tutaj nie ma ciekawego.');
 header("Location: javascript://history.go(-1); Location.reload()");*/
-session_start();
-if (!isset($_GET['follow_id']) or !isset($_SESSION['z1'])) header('Location: javascript:history.go(-1); Location.reload()');
-else {
-    echo $_GET['follow_id'];
-    array_walk($_SESSION, function(&$value, $key) {
-       echo "<p>$key -> $value</p>";
-    });
+try {
+    session_start();
+    if (!isset($_GET['follow_id']) or !isset($_SESSION['z1'])) header('Location: javascript:history.go(-1); Location.reload()');
+    else {
+        echo $_GET['follow_id'];
+        array_walk($_SESSION, function (&$value, $key) {
+            echo "<p>$key -> $value</p>";
+        });
+    }
+} catch (Exception $e) {
+    echo $e;
 }
 ?>
 <h1 class="text">Funkcja obserwowania jest obecnie przepisywana. Zajrzyj ponownie później!</h1>
