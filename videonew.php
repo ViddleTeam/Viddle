81b7a7a1df4ccf26b744a2c850029586c94b5350
MinecraftCiach <ciach795@gmail.com>
2021-03-18 10:38:42 +0100

    Update

--- videonew.php
+++ videonew.php
@@ -9,6 +9,7 @@ $video_found = false;
 $stmt = sprintf("SELECT * FROM viddle_videos WHERE video_id = '%s'", $db->real_escape_string($id));
 $video = [];
 $publisher = [];
+$is_following = false;
 if ($result = @$db->query($stmt)) {
     if ($result->num_rows == '1') $video_found = true;
     else $video_found = false;
@@ -26,7 +27,7 @@ if ($result = @$db->query($stmt)) {
  * publishdate -> published_at
  * opis -> description
  * fname -> filename
- */
+*/
 if ($result = @$db->query("SELECT * FROM viddle_users WHERE uid")) {
     $publisher = $result->fetch_assoc();
 }
@@ -35,39 +36,68 @@ if ($result = @$db->query("SELECT * FROM viddle_users WHERE uid")) {
  * login -> username
  * observators -> followers
  * avatarname -> avatar
- */
+*/
 $avatar_url = null;
 if ($publisher['avatarname'] == 'x') {
     $avatar_url = 'anonim.png';
 } else {
     $avatar_url = "https://cdn.viddle.xyz/cdn/videos/avatars/${publisher['uid']}/${publisher['uid']}.${publisher['avatarname']}";
 }
+$userid = $_SESSION['id'];
+$followers = $db->query("SELECT * FROM viddle_followers WHERE followed='$userid'");
+$followcount = $followers->num_rows;
+if ($_SESSION['z1'] == true) {
+    $uid = $_SESSION['uid'];
+    $test = $db->query("SELECT * FROM viddle_followers WHERE followed='$userid' AND following='$uid'");
+    $followcount = $followers->num_rows;
+    if ($followcount == 1) {
+        $is_following = true;
+    } else {
+        $is_following = false;
+    }
+} else {
+    $is_following = false;
+}
 ?>
 <div style="margin-top: 70px; justify-content: center;" class="container">
     <?php if ($video_found) { ?>
-        <div class="row">
-            <div class="col-md-7">
-                <iframe src="https://cdn.plrjs.com/player/3ehs9nbnddz9a/or43an4e4gpx.html?file=https://cdn.viddle.xyz/cdn/videos/videos/<?= $video['video_id'].'/'.$video['fname'] ?>&title<?= $video['title'] ?>&autoplay=1" style="width: 100%; height: 360px;" frameborder="0" allowfullscreen></iframe>
-                <div class="card-videoch" style="margin-top: 10px; padding: 12px; cursor: default; width: 100%;">
-                    <h4 class="text-truncate"><?= $video['title'] ?></h4>
-                    <div class="container row" style="margin-top: 20px;">
+    <div class="row">
+        <div class="col-md-7">
+            <iframe src="https://cdn.plrjs.com/player/3ehs9nbnddz9a/or43an4e4gpx.html?file=https://cdn.viddle.xyz/cdn/videos/videos/<?=$video['video_id'] . '/' . $video['fname'] ?>&title<?=$video['title'] ?>&autoplay=1" style="width: 100%; height: 360px;" frameborder="0" allowfullscreen></iframe>
+            <div class="card-videoch" style="margin-top: 10px; padding: 12px; cursor: default; width: 100%;">
+                <h4 class="text-truncate"><?=$video['title'] ?></h4>
+                <div class="container row" style="margin-top: 20px;">
                         <span style="margin-left: 10px;">
-                            <a href="channel?id=<?= $publisher['uid'] ?>">
-                                <img width="48px" style="border-radius: 50%; margin-right: 5px;" class="img-responsive d-none d-md-block" src="<?= $avatar_url ?>">
+                            <a href="channel?id=<?=$publisher['uid'] ?>">
+                                <img width="48px" style="border-radius: 50%; margin-right: 5px;" class="img-responsive d-none d-md-block" src="<?=$avatar_url ?>">
                             </a>
                         </span>
-                        <span style="margin-left: 10px; margin-right: auto;">
+                    <span style="margin-left: 10px; margin-right: auto;">
                             <h5 style="align-items: center;">
-                                <a href="channel?id=<?= $publisher['uid'] ?>"><?= $publisher['login'] ?></a>
+                                <a href="channel?id=<?=$publisher['uid'] ?>"><?=$publisher['login'] ?></a>
                             </h5>
-                            <p style="text-align: left; margin-bottom: 20px; margin-top: -6px;"><?= $publisher['observators'] ?> obserwujących</p>
+                            <p style="text-align: left; margin-bottom: 20px; margin-top: -6px;"><?=$publisher['observators'] ?> obserwujących</p>
                         </span>
-                        <span style="margin-left: auto; margin-right: -20px;">
-
+                    <span style="margin-left: auto; margin-right: -20px;">
+                        <?php
+                            if ($_SESSION['uid'] != $publisher['publisher']) {
+                            if ($_SESSION['z1'] === true) {
+                                if ($is_following === true) { ?>
+                                    <button href="/follow?followid=<?=$publisher['publisher'] ?>" class="btn btn-primary" style="padding: 10px; background-color: #808080;">Obserwujesz</button>
+                                    <?php
+                                } else { ?>
+                                    <button href="/follow?followid=<?=$publisher['publisher'] ?>" class="btn btn-primary" style="padding: 10px;">Obserwuj</button>
+                                    <?php
+                                }
+                            } else {
+                                ?>
+                                <button type="button" class="btn btn-primary" style="padding: 10px;" data-toggle="modal" data-target="#loginModal">Obserwuj</button>
+                                <?php
+                            } ?>
                         </span>
-                    </div>
                 </div>
             </div>
         </div>
-    <?php } ?>
+    </div>
+<?php }} ?>
 </div>
