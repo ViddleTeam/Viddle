c1f75af1bd7c136d5d71ad125cf31a76372c7f29
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-03-20 19:09:12 +0100

    poprawki

--- index.php
+++ index.php
@@ -282,38 +282,7 @@ if ($randomviewsthree > 999 && $randomviewsthree <= 999999) {
 <?php if (isset($_SESSION['ver'])) {
 	unset($_SESSION['ver']);
 }
+require_once('partials/footer.php');
 $connect->close();
 ?>
-<div class="modal fade" id="supportModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-  aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content bg-dark">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Wesprzyj rozwój Viddle</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        Możesz wesprzeć rozwój serwisu na jeden z 3 sposobów:<br>
-        <ul>
-          <li>Tipply: <a href="https://tipply.pl/u/SlaVistaPL">kliknij tutaj</a></li>
-          <li>Bitcoin: bc1qvtr3mr77tzfm552rm6zxyrc7ry3ct462672wan</li>
-          <li>Ethereum: 0xF3FdE269e853CEA2F5C385F410e13C8a57d00479</li>
-        </ul>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal" style="padding: 10px;">Zamknij</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-<footer class="page-footer font-small" style="background-color: #212121; margin-top: 30px; width: 100%;">
-  <div class="footer-copyright text-center text-white py-3">
-      &copy; 2020-2021 Viddle Developers<br>
-      <a href="/attachments/privacypolicy.pdf">Polityka prywatności</a> • <a href="/attachments/termsofservice.pdf">Regulamin</a> • <a href="https://discord.gg/QsrbDtxWpn">Serwer Discord</a> • <a data-toggle="modal" data-target="#supportModal">Wesprzyj rozwój Viddle</a><br>
-      Kontakt mailowy: <b>support@viddle.xyz</b>
-  </div>
-</footer>
 </body></html>
