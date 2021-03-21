cb168ce48ae8a148d9851b5020c3172a563201b9
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-03-21 18:37:45 +0100

    bug fix w rejestracji

--- register.php
+++ register.php
@@ -82,7 +82,7 @@ if (isset($login)){
                 $email_e = $connect->real_escape_string($email);
                 $uid = uniqid(uniqid());
                 //$success = $connect->query("INSERT INTO viddle_users VALUES ('$login', '$haslo_h', , 1, 0, 0, 0, 0)");
-                $success = $connect->query("INSERT INTO viddle_users VALUES ('$login_e', '$haslo_h', '$email_e', '$uid', 1, 0, 0, 0, 'x', 'x', NULL, 0)");
+                $success = $connect->query("INSERT INTO viddle_users VALUES ('$login_e', '$haslo_h', '$email_e', '$uid', 1, 0, 0, 0, 'x', 'x', NULL, 0, '0')");
                 if ($success) {
                     $_SESSION['z'] = true;
                     header('Location: prejestracja.php');
