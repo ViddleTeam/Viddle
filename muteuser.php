60ef41dfcd0f036159929469d51b90042518437f
smieszneyt <67741022+smieszneyt@users.noreply.github.com>
2021-03-24 16:47:51 +0100

    Create muteuser.php

---
+++ muteuser.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="/cdn-cgi/apps/head/KXVv_VQyBtibcmvK-FYml_HDUsM.js"></script><link rel="stylesheet" href="https://midacss.ml/assets/master.min.css" />
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Panel moderatora - usuwanie filmu</title>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+<link rel="stylesheet" href="style.css">
+<meta property="og:title" content="Viddle">
+<meta property="og:description" content="Filmy, muzyka i wiele więcej. Udostępniaj swoje filmy znajomym, rodzinie, oraz całemu światu, za pomocą Viddle.">
+<script src="script.js"></script>
+<script src="https://kit.fontawesome.com/604acb9c5e.js"></script>
+<link rel="stylesheet" href="https://kit-free.fontawesome.com/releases/latest/css/free.min.css" media="all"><link rel="stylesheet" href="https://kit-free.fontawesome.com/releases/latest/css/free-v4-font-face.min.css" media="all"><link rel="stylesheet" href="https://kit-free.fontawesome.com/releases/latest/css/free-v4-shims.min.css" media="all">
+</head>
+<body class="bg-img img-fluid" style="align-items: center; justify-content: center; text-align: center;">
+<div class="card-login" style="width: 550px; height: auto; align-items: center; padding: 15px 0 15px 0;">
+<div class="card-body">
+<h3 style="font-weight: bold;">Wycisz użytkownika</h3>
+<p>Wycisz jakąkolwiek osobe na viddle</p><br>
+<form method="post">
+<div class="md-form input-group mb-3" style="margin: auto; width: 100%">
+<input type="text" name="id" class="form-control" placeholder="id użytkownika" aria-label="Id filmu" aria-describedby="material-addon1">
+</div>
+  <div class="md-form input-group mb-3" style="margin: auto; width: 100%">
+<input type="text" name="h" class="form-control" placeholder="Czas wyciszenia (w godzinach)" aria-label="Czas wyciszenia (w godzinach)" aria-describedby="material-addon1">
+</div>
+<div class="md-form input-group mb-3" style="margin: auto; width: 100%">
+<input type="text" name="uzasadnienie" class="form-control" placeholder="uzasadnienie" aria-label="uzasadnienie" aria-describedby="material-addon1">
+</div><br>
+<center>
+<div class="container row" style="justify-content: center;">
+<input type="submit" value="Wycisz użytkownika" name="usun" class="btn btn-success" style="padding: 10px; color: white;">
+</form>
+</div>
+</center>
+</div>
+</div><br>
+</body>
+</html>
