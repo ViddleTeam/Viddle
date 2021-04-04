9a54f129b267f8bec1807b9d5da124ba988bbcc5
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-04-04 13:46:24 +0200

    Update comingsoon.php

--- comingsoon.php
+++ comingsoon.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
     <head>
         <style>
@@ -58,67 +59,41 @@
             }
         </style>
         <script src="https://kit.fontawesome.com/604acb9c5e.js"></script>
+        <script async src="https://arc.io/widget.min.js#YXGBLYWj"></script>
+        <script src="assets/script.js"></script>
         <title>Viddle | Już wkrótce!</title>
+        <link rel="shortcut icon" href="/logo.png">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="UTF-8" />
         <meta property="og:title" content="Viddle - już wkrótce!" />
         <meta property="og:description" content="Do czasu, kiedy Viddle zostanie uruchomiony, będziesz mógł zobaczyć jedynie tą stronę. Ale bez obaw, możesz obserwować nasze poczynania na innych portalach społecznościowych. Kliknij jeden z linków na stronie, żeby zobaczyć nasze oficjalne profile i/lub wejść na nasz serwer Discord." />
-        <link rel="stylesheet" href="https://cdn.patryqhyper.pl/vdp/mdb/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdn.patryqhyper.pl/vdp/mdb/css/mdb.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
         <script src="https://kit.fontawesome.com/604acb9c5e.js"></script>
         <link rel="stylesheet" href="https://kit-free.fontawesome.com/releases/latest/css/free.min.css" media="all">
+	<script data-ad-client="ca-pub-4393741826344878" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     </head>
     <body>
         <div class="container">
         <img src="https://cdn.discordapp.com/attachments/719598185118433311/766951476341506048/1602925873953.png" width="200px" /><br><br>
-        <h1>Twoje wirtualne repozytorium filmów.</h1>
-        <h3 style="font-weight: normal;">Już wkrótce.</h3><br>
+        <h3 style="font-weight: normal;">Projekt zamknięty</h3><br>
         <p>
-            Do czasu, kiedy Viddle zostanie uruchomiony, będziesz mógł zobaczyć jedynie ten ekran.
-            Ale bez obaw, możesz obserwować nasze poczynania na innych portalach społecznościowych.
-            Kliknij jeden z linków, żeby zobaczyć nasze oficjalne profile i/lub wejść na nasz
-            serwer Discord.
+            Z dniem 4 kwietnia 2021 Viddle zostało zamknięte. Dziękuję wszystkim osobom, które zaangażowały się w istnienie projektu. 
+            Deweloperom, użytkownikom, krytykom, komukolwiek kto się w ten projekt zaangażował.<br>
         </p><br>
-        <div class="social-media">
-            <a href="https://twitter.com/viddlelive"><i class="fab fa-twitter fa-2x scmedia"></i></a>
-            <a href="https://www.facebook.com/viddlelive"><i class="fab fa-facebook fa-2x scmedia" style="margin-left: 15px;"></i></a>
-            <a href="https://www.instagram.com/viddlelive/"><i class="fab fa-instagram-square fa-2x scmedia" style="margin-left: 15px;"></i></a>
-            <a href="https://discord.gg/QsrbDtxWpn"><i class="fab fa-discord fa-2x scmedia" style="margin-left: 15px;"></i></a>
-        </div>
     </div>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <svg xmlns="https://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path fill="#e7698b" fill-opacity="1" d="M0,256L48,229.3C96,203,192,149,288,154.7C384,160,480,224,576,218.7C672,213,768,139,864,128C960,117,1056,171,1152,197.3C1248,224,1344,224,1392,224L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"/>
     </svg>
-    <div class="container-inside" style="text-align: center;">
-        <h2>Co zyskasz dzięki dołączeniu na Viddle?</h2>
-        <center>
-            <div class="container row" style="text-align: center; justify-content: center; color: white !important;">
-                <span style="max-width: 24rem; margin: 5px;">
-                    <i class="fas fa-film fa-4x" style="margin-bottom: 5px;"></i><br>
-                    <h4>Większa popularność</h4>
-                    <p>Dzięki algorytmom podpowiadającym filmy na portalu, każdy będzie mógł zaistnieć w sieci.</p>
-                </span>
-                <span style="max-width: 24rem; margin: 5px;">
-                    <i class="fas fa-user-friends fa-4x" style="margin-bottom: 5px;"></i><br>
-                    <h4>Filmy dopasowane do twoich zainteresowań</h4>
-                    <p>Im częściej oglądasz Viddle, tym częściej algorytm podpowiada tobie filmy, które mogłyby cię zainteresować.</p>
-                </span>
-                <span style="max-width: 24rem; margin: 5px;">
-                    <i class="fas fa-plug fa-4x" style="margin-bottom: 5px;"></i><br>
-                    <h4>Logowanie jednym kliknięciem</h4>
-                    <p>Wpisz swoje dane do konta jeden raz, a potem wystarczy żebyś kliknął przycisk autoryzacji, żeby przejść do kanału.</p>
-                </span>
-            </div>
-        </center>
-    </div>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="secondpart">
         <path fill="#e7698b" fill-opacity="1" d="M0,256L48,229.3C96,203,192,149,288,154.7C384,160,480,224,576,218.7C672,213,768,139,864,128C960,117,1056,171,1152,197.3C1248,224,1344,224,1392,224L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"/>
     </svg>
     <br>
-    <center>
-        <p style="text-align: center;">
-            &copy; 2020 - <?php echo date('Y') ?> Viddle Team
-        </p>
-    </center>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
     </body>
 </html>
